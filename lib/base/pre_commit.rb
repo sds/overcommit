20aa50681399369194e88115aae1cee3d4517b5d
Aiden Scandella <aiden@causes.com>
2012-07-15 22:19:37 -0700

    Port css_linter, test_history
    Change-Id: I2cadb50dd6bc525bd04b064477fc458e1965409f
    Reviewed-on: https://gerrit.causes.com/9807
    Reviewed-by: Aiden Scandella <aiden@causes.com>
    Tested-by: Aiden Scandella <aiden@causes.com>

--- lib/base/pre_commit.rb
+++ lib/base/pre_commit.rb
@@ -15,28 +15,6 @@ module Causes
   class PreCommitHook
     include GitHook
 
-    def initialize
-      super
-      # Only check test history if repo supports it
-      # unless FileTest.exist?('spec/support/record_results_formatter.rb')
-      #   @checks.delete 'test_history'
-      # end
-    end
-
-    CSS_LINTER_PATH = File.join(SCRIPTS_PATH, 'csslint-rhino.js')
-    def check_css_linter
-      staged = staged_files('css')
-      return :good, nil if staged.empty?
-
-      return :warn, "Rhino is not installed" unless in_path? 'rhino'
-
-      paths = staged.map { |s| s.path }.join(' ')
-
-      output = `rhino #{CSS_LINTER_PATH} --quiet --format=compact #{paths} | grep 'Error - '`
-      staged.each { |s| output = s.filter_string(output) }
-      return (output !~ /Error - (?!Unknown @ rule)/ ? :good : :bad), output
-    end
-
     def check_ruby_syntax
       clean = true
       output = []
@@ -71,51 +49,5 @@ module Causes
       return (clean ? :good : :bad), output
     end
 
-    TEST_RESULTS_FILE = '.spec-results'
-    def check_test_history
-      output = []
-      relevant_tests =
-        `relevant-tests -- #{modified_files.join(' ')} > /dev/null 2>&1`.
-        split("\n")
-      relevant_tests = relevant_tests.map { |r| File.expand_path r }
-      unless relevant_tests.any?
-        return :warn, 'No relevant tests for this change...write some?'
-      end
-
-      begin
-        good_tests = File.open(TEST_RESULTS_FILE, 'r').readlines.map do |spec_file|
-          File.expand_path spec_file.strip
-        end
-      rescue Errno::ENOENT
-        good_tests = []
-      end
-
-      unless good_tests.any?
-        return :bad,
-          'The relevant tests for this change have not yet been run using `specr`'
-      end
-
-      missed_tests = (relevant_tests - good_tests)
-      unless missed_tests.empty?
-        output << 'The following relevant tests have not been run recently:'
-        output << missed_tests.sort
-        return :bad, output
-      end
-
-      # Find files modified after the tests were run
-      test_time = File.mtime(TEST_RESULTS_FILE)
-      untested_files = modified_files.reject do |file|
-        File.mtime(file) < test_time
-      end
-
-      unless untested_files.empty?
-        output << 'The following files were modified after `specr` was run.'
-        output << '(their associated tests may be broken):'
-        output << untested_files.sort
-        return :bad, output
-      end
-
-      return :good
-    end
   end
 end
---
+++ lib/plugins/pre_commit/css_linter.rb
@@ -0,0 +1,18 @@
+module Causes::GitHook
+  class CssLinter < HookSpecificCheck
+    include HookRegistry
+    file_type :css
+
+    CSS_LINTER_PATH = File.join(SCRIPTS_PATH, 'csslint-rhino.js')
+
+    def run_check
+      return :warn, "Rhino is not installed" unless in_path? 'rhino'
+
+      paths = staged.map { |s| s.path }.join(' ')
+
+      output = `rhino #{CSS_LINTER_PATH} --quiet --format=compact #{paths} | grep 'Error - '`
+      staged.each { |s| output = s.filter_string(output) }
+      return (output !~ /Error - (?!Unknown @ rule)/ ? :good : :bad), output
+    end
+  end
+end
