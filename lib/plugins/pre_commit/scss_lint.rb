311af8a1c8c2dc84ec6449e60aea45ded7197697
Shane da Silva <shane@causes.com>
2012-12-18 10:48:14 -0800

    Add SCSS lint hook
    Adds a pre-commit hook that checks SCSS files for syntax errors and
    other various lint issues.
    
    Change-Id: Iff0ae57a2db27ddb3ff5c8e4d719d264093cb553
    Reviewed-on: https://gerrit.causes.com/16586
    Reviewed-by: Shane da Silva <shane@causes.com>
    Tested-by: Shane da Silva <shane@causes.com>

---
+++ lib/plugins/pre_commit/scss_lint.rb
@@ -0,0 +1,21 @@
+module Causes::GitHook
+  class ScssLint < HookSpecificCheck
+    include HookRegistry
+    file_type :scss
+
+    def run_check
+      begin
+        require 'scss-lint'
+      rescue LoadError
+        return :warn, 'scss-lint not installed -- run `gem install scss-lint`'
+      end
+
+      paths = staged.map { |s| s.path }.join(' ')
+
+      output = `scss-lint #{paths} 2>&1`
+
+      staged.each { |s| output = s.filter_string(output) }
+      return (output.empty? ? :good : :bad), output
+    end
+  end
+end
