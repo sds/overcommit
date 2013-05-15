f81cf464c2cd7e2d811c61743a7a402c32c03ae5
Aiden Scandella <aiden@causes.com>
2013-05-15 13:21:34 -0700

    Address style cleanups in Utils
    As pointed out by Joe on
    https://gerrit.causes.com/#/c/22332/2/lib/overcommit/utils.rb
    
    Change-Id: I5cc01a344f2b9ca9098969b72ff21ad3c91eb430
    Reviewed-on: https://gerrit.causes.com/22356
    Reviewed-by: Aiden Scandella <aiden@causes.com>
    Tested-by: Aiden Scandella <aiden@causes.com>

--- lib/overcommit/utils.rb
+++ lib/overcommit/utils.rb
@@ -4,7 +4,7 @@ module Overcommit
       include ConsoleMethods
 
       def hook_name
-        File.basename($0).gsub('-', '_')
+        File.basename($0).tr('-', '_')
       end
 
       def load_hooks
@@ -22,9 +22,9 @@ module Overcommit
       # http://stackoverflow.com/questions/1509915/converting-camel-case-to-underscore-case-in-ruby
       def underscorize(str)
         str.gsub(/::/, '/').
-            gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-            gsub(/([a-z\d])([A-Z])/,'\1_\2').
-            tr("-", "_").
+            gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+            gsub(/([a-z\d])([A-Z])/, '\1_\2').
+            tr('-', '_').
             downcase
       end
     end
