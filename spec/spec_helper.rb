4e01bbb3b0ebb052d52cab28f77bf2f6136c0a22
Aiden Scandella <aiden@causes.com>
2013-05-29 14:04:42 -0700

    Configure rspec for documentation + color
    This provides nicer output in e.g. jenkins.
    
    Change-Id: I422249b7e227c3e2064eaf1ad7186a271ada5597
    Reviewed-on: https://gerrit.causes.com/23045
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require 'overcommit'
 
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.tty = true
+  config.formatter = :documentation
+end
+
 def exit(*args) ; end
 
 # Silence output to STDOUT
