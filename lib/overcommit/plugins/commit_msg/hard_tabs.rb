c2bd831c18cf0956a738189e3dc4a4d34942d36a
Aiden Scandella <sectioneightinc@gmail.com>
2013-06-09 13:44:34 -0700

    Don't lint hard tabs in comments
    The default git commit message includes a 'Changes to be committed'
    section which has hard tabs (ghasp) in comments.
    
    We shouldn't warn the user about these tabs. This runs
    `user_commit_message`, which only includes non-comment lines (in
    addition to stripping git diff output when running `git commit
    --verbose`)
    
    Change-Id: Ib9163a55508c448c745b5aa5bd54508511ea7b03
    Reviewed-on: https://gerrit.causes.com/23511
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Lann Martin <lann@causes.com>
    Reviewed-by: Greg Hurrell <greg@causes.com>

--- lib/overcommit/plugins/commit_msg/hard_tabs.rb
+++ lib/overcommit/plugins/commit_msg/hard_tabs.rb
@@ -3,8 +3,8 @@ module Overcommit::GitHook
     include HookRegistry
 
     def run_check
-      # Catches hard tabs
-      if commit_message.join.index /\t/
+      # Catches hard tabs entered by the user (not auto-generated)
+      if user_commit_message.join.index /\t/
         return :warn, "Don't use hard tabs in commit messages."
       end
 
