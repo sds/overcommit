e0003746daea809df256ed661b229abfdc0726a4
Shane da Silva <shane@causes.com>
2013-07-19 19:53:38 -0700

    Simplify repo helper in GitSpecHelpers
    Condense two separate calls to `Dir.chdir(path)` into a single call with
    a block passed in.
    
    Change-Id: I41b089981414d4f02757740e411f336d5e18102a
    Reviewed-on: https://gerrit.causes.com/25222
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

--- spec/support/git_spec_helpers.rb
+++ spec/support/git_spec_helpers.rb
@@ -1,8 +1,10 @@
 module GitSpecHelpers
   def repo(&block)
     Dir.mktmpdir.tap do |path|
-      Dir.chdir(path) { `git init` }
-      Dir.chdir(path, &block)
+      Dir.chdir(path) do
+        `git init`
+        block.call
+      end
     end
   end
 
