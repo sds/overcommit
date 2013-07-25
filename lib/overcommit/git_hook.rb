sds/overcommit

commit ce42f5a03eb871e7c95a7714af5133a14703b829

Syntax check rake files
I added the ability to specify multiple file types for an overcommit
check, and made it check ruby style and syntax on rake files as well.

Change-Id: Ib737af6c9d9a8fd3b66c2ce0558e1bb51f3a7d85
Reviewed-on: https://gerrit.causes.com/25541
Tested-by: jenkins <jenkins@causes.com>
Reviewed-by: Shane da Silva <shane@causes.com>

lib/overcommit/hook_specific_check.rb
@@ -52,7 +52,8 @@ module Overcommit
 
       def staged
         @staged ||= modified_files.map do |filename|
-          if self.class.filetype.nil? || filename.end_with?(".#{self.class.filetype}")
+          filetypes = Array(self.class.filetypes).map { |type| ".#{type}" }
+          if filetypes.empty? || filename.ends_with?(*filetypes)
             StagedFile.new(filename)
           end
         end.compact
@@ -91,8 +92,11 @@ module Overcommit
           take_while { |line| !line.start_with?('diff --git') }
       end
 
-      def self.file_type(type)
-        self.filetype = type
+      class << self
+        def file_type(*types)
+          self.filetype = types
+        end
+        alias file_types file_type
       end
     end
   end

lib/overcommit/plugins/pre_commit/ruby_style.rb
@@ -1,7 +1,7 @@
 module Overcommit::GitHook
   class RubyStyle < HookSpecificCheck
     include HookRegistry
-    file_type :rb
+    file_types :rb, :rake
 
     def run_check
       unless in_path?('rubocop')

lib/overcommit/plugins/pre_commit/ruby_syntax.rb
@@ -1,7 +1,7 @@
 module Overcommit::GitHook
   class RubySyntax < HookSpecificCheck
     include HookRegistry
-    file_type :rb
+    file_types :rb, :rake
 
     def run_check
       clean = true

commit 89febeb2e030587d342f1b482bbcf40031cb2a66

Fix file type filtering for staged files
Commit ce42f5a introduced a bug through the use of `ends_with?` instead
of `end_with?` and referencing a non-existent `filetypes` attribute.

This change corrects the use of `end_with?` and renames the class
`filetype` attribute to `filetypes` to reduce confusion, since it
technically contains an array of extensions instead of a single file
type now.

Change-Id: Ief7df9f925f19308873a39a464e6a355a2ffb07f
Reviewed-on: https://gerrit.causes.com/25555
Tested-by: jenkins <jenkins@causes.com>
Reviewed-by: Aaron Neyer <aaron.neyer@causes.com>

lib/overcommit/git_hook.rb
@@ -27,7 +27,7 @@ module Overcommit
 
           # Ignore a check if it only applies to a specific file type and there
           # are no staged files of that type in the tree
-          next if check_class.filetype && check.staged.empty?
+          next if check_class.filetypes && check.staged.empty?
 
           reporter.with_status(check) do
             run_and_filter_check(check)

lib/overcommit/hook_specific_check.rb
@@ -13,7 +13,7 @@ module Overcommit
 
     class HookSpecificCheck
       class << self
-        attr_accessor :filetype, :stealth, :required
+        attr_accessor :filetypes, :stealth, :required
 
         def stealth!
           self.stealth = true
@@ -53,7 +53,7 @@ module Overcommit
       def staged
         @staged ||= modified_files.map do |filename|
           filetypes = Array(self.class.filetypes).map { |type| ".#{type}" }
-          if filetypes.empty? || filename.ends_with?(*filetypes)
+          if filetypes.empty? || filename.end_with?(*filetypes)
             StagedFile.new(filename)
           end
         end.compact
@@ -94,7 +94,7 @@ module Overcommit
 
       class << self
         def file_type(*types)
-          self.filetype = types
+          self.filetypes = types
         end
         alias file_types file_type
       end