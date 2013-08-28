e38eabecacb2ce23d13e1863b04395ee94ad161d
John Skopis <john.skopis@causes.com>
2013-08-28 11:40:29 -0700

    Add config to RESTRICTED_PATHS
    We want to add the 'config' directory to the array of RESTRICTED_PATHS
    in order to prevent accidental modifications to config files.
    
    Change-Id: I1b1d0ef6e840219e5d8285a33e342fac6cd28b05
    Reviewed-on: https://gerrit.causes.com/27592
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Kim Douglas Clarence Mason <kim.mason@causes.com>

--- lib/overcommit/plugins/pre_commit/restricted_paths.rb
+++ lib/overcommit/plugins/pre_commit/restricted_paths.rb
@@ -1,7 +1,7 @@
 module Overcommit::GitHook
   class RestrictedPaths < HookSpecificCheck
     include HookRegistry
-    RESTRICTED_PATHS = %w[vendor]
+    RESTRICTED_PATHS = %w[vendor config]
 
     def run_check
       RESTRICTED_PATHS.each do |path|
