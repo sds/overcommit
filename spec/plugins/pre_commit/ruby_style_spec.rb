sds/overcommit

commit e06c032651a06f01744836b83aa5840f1efc2873

Use enum_for with ascend in rubocop_yml_for
It turns out that even though `Pathname#ascend` wasn't written to return
an enumerator when no block was given, you can use a handy-dandy helper
called `enum_for` to wrap the call in an enumerator.

This allows the method to be drastically simplified to use `map` and
`find`.

Change-Id: Idda85f38ffd0e683334a20cdd114c5d4234d8d66
Reviewed-on: https://gerrit.causes.com/30175
Tested-by: jenkins <jenkins@causes.com>
Reviewed-by: Lann Martin <lann@causes.com>
Reviewed-by: Greg Hurrell <greg@causes.com>
Reviewed-by: Joe Lencioni <joe.lencioni@causes.com>

lib/overcommit/plugins/pre_commit/ruby_style.rb
@@ -1,3 +1,4 @@
+require 'enumerator'
 require 'pathname'
 
 module Overcommit::GitHook
@@ -54,15 +55,10 @@ module Overcommit::GitHook
     end
 
     def rubocop_yml_for(staged_file)
-      config_file = nil
-      Pathname.new(staged_file.original_path).ascend do |path|
-        rubo_file = path + '.rubocop.yml'
-        if rubo_file.file?
-          config_file = rubo_file
-          break
-        end
-      end
-      config_file
+      Pathname.new(staged_file.original_path).
+               enum_for(:ascend).
+               map { |path| path + '.rubocop.yml' }.
+               find { |path| path.file? }
     end
   end
 end

commit 2fdedf11f9f4ef6efd459931989c381bc2e850f4

Fix rubocop.yml not found in working directory
The switch to using `Pathname#ascend` in e06c0326 broke finding a
`.rubocop.yml` file in the current working directory, since the path
passed to `ascend` would usually be relative (i.e. have no leading `/`),
and so we would not generate a final path of just searching for
`.rubocop.yml`.

Change-Id: I7e66727d077093e3f7c77a03348f488d7e334371
Reviewed-on: https://gerrit.causes.com/30476
Tested-by: jenkins <jenkins@causes.com>
Reviewed-by: Shane da Silva <shane@causes.com>

lib/overcommit/plugins/pre_commit/ruby_style.rb
@@ -55,10 +55,13 @@ module Overcommit::GitHook
     end
 
     def rubocop_yml_for(staged_file)
-      Pathname.new(staged_file.original_path).
-               enum_for(:ascend).
-               map { |path| path + '.rubocop.yml' }.
-               find { |path| path.file? }
+      possible_files(staged_file.original_path).find { |path| path.file? }
+    end
+
+    def possible_files(file_path)
+      files = Pathname.new(file_path).enum_for(:ascend).
+                       map { |path| path + '.rubocop.yml' }
+      files << Pathname.new('.rubocop.yml')
     end
   end
 end

spec/plugins/pre_commit/ruby_style_spec.rb
@@ -1,0 +1,40 @@
+require 'spec_helper'
+
+describe Overcommit::GitHook::RubyStyle do
+  let(:ruby_style) { subject }
+
+  describe '#possible_files' do
+    it 'returns all possible paths for the rubocop config file' do
+      result = ruby_style.send(:possible_files, 'foo/bar')
+      result.map { |path| path.to_s }.
+        should eq %w[foo/bar/.rubocop.yml foo/.rubocop.yml .rubocop.yml]
+    end
+  end
+
+  describe '#rubocop_yml_for' do
+    let(:staged_file) { stub(original_path: 'some/dir') }
+    let(:existing) { stub(file?: true) }
+    let(:non_existing) { stub(file?: false) }
+
+    it 'returns the file that exists' do
+      ruby_style.should_receive(:possible_files).
+                 with('some/dir').
+                 and_return([existing, non_existing])
+      ruby_style.send(:rubocop_yml_for, staged_file).should eq existing
+    end
+
+    it 'returns nil if none of the files exist' do
+      ruby_style.stub(possible_files: [non_existing])
+      ruby_style.send(:rubocop_yml_for, staged_file).should be_nil
+    end
+  end
+
+  describe '#rubocop_config_mapping' do
+    it 'inserts the staged files into a hash' do
+      file = stub(path: 'foobar/file.x')
+      ruby_style.stub(staged: [file])
+      ruby_style.should_receive(:rubocop_yml_for).with(file).and_return('boing')
+      ruby_style.send(:rubocop_config_mapping).should eq('boing' => ['foobar/file.x'])
+    end
+  end
+end