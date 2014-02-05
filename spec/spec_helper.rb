44101b95a95e58b5aace7b9049c6ebecb0d38914
Shane da Silva <shane@dasilva.io>
2014-02-04 22:46:05 -0800

    Remove commented lines from spec_helper
    Change-Id: I75282aa31c37a56be811894d751c4f801e54de0f
    Reviewed-on: http://gerrit.causes.com/35577
    Reviewed-by: Shane da Silva <shane@causes.com>
    Tested-by: Shane da Silva <shane@causes.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,15 +16,5 @@ end
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
 
 RSpec.configure do |config|
-  #config.color_enabled = true
-  #config.tty = true
-  #config.formatter = :documentation
-
   config.include GitSpecHelpers
-
-  # For commit message check tests, just specify the commit_msg for each context
-  #config.before :each,
-                #:example_group => { :file_path => %r{\bspec/plugins/commit_msg/} } do
-    #subject.stub(:raw_commit_message).and_return(commit_msg.split("\n"))
-  #end
 end
