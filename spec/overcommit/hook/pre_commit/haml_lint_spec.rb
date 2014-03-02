ac1aa66be19e18f8313272f22c9f70f4b940a8a9
Guo Xiang Tan <tgx_world@hotmail.com>
2014-03-02 10:02:39 -0800

    Modify HamlLint to exclude RubyScript hook.

--- spec/overcommit/hook/pre_commit/haml_lint_spec.rb
+++ spec/overcommit/hook/pre_commit/haml_lint_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
 describe Overcommit::Hook::PreCommit::HamlLint do
   let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
   let(:context) { double('context') }
+  let(:staged_file) { 'filename.haml' }
+  let(:applicable_files) { [staged_file, 'file2.haml'] }
   subject { described_class.new(config, context) }
 
   before do
     subject.stub(:in_path?).and_return(true)
-    subject.stub(:applicable_files).and_return(%w[file1.haml file2.haml])
+    subject.stub(:applicable_files).and_return(applicable_files)
   end
 
   context 'when haml-lint is not installed' do
@@ -16,6 +18,22 @@ describe Overcommit::Hook::PreCommit::HamlLint do
     end
 
     it { should warn }
+  end
+
+  context 'RubyScript should be excluded' do
+    before do
+      result = double('result')
+      result.stub(:success?).and_return(true)
+      subject.stub(:command).and_return(result)
+    end
+
+    it 'should run the right command' do
+      expect(subject)
+        .to receive(:command)
+        .with("haml-lint --exclude-lint RubyScript #{applicable_files.join(' ')}")
+
+      expect(subject).to_not warn
+    end
   end
 
   context 'when haml-lint exits successfully' do
