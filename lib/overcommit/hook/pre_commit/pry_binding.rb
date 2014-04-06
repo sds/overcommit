439d12ed0e8632fe8eecaa1474659b029d7bccb6
Jeff Felchner <accounts+git@thekompanee.com>
2014-04-06 02:28:26 -0500

    Add PryBinding pre-commit hook
    Change-Id: Ie71e7db0d6ae06ee43c2f195b4939bc8945a7587
    Reviewed-on: http://gerrit.causes.com/37951
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

---
+++ lib/overcommit/hook/pre_commit/pry_binding.rb
@@ -0,0 +1,14 @@
+module Overcommit::Hook::PreCommit
+  # Adds a check to make sure no `binding.pry`'s have been left in the code
+  class PryBinding < Base
+    def run
+      result = execute(%w[grep -IHnE ^\s*binding\.pry] + applicable_files)
+
+      unless result.stdout.empty?
+        return :bad, "Found a binding.pry left in:\n#{result.stdout}"
+      end
+
+      :good
+    end
+  end
+end
