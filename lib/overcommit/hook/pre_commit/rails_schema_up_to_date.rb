bc7d48087df46c63185b3869e70a39172d1dcc4d
Jeff Felchner <accounts+git@thekompanee.com>
2014-04-06 00:07:03 -0500

    Initial Untested Rails Schema Check Hook

---
+++ lib/overcommit/hook/pre_commit/rails_schema_up_to_date.rb
@@ -0,0 +1,36 @@
+module Overcommit::Hook::PreCommit
+  # Check to see whether the schema file is in line with the migrations
+  class RailsSchemaUpToDate < Base
+    def run
+      if migration_files.any? && schema_files.none?
+        return :bad, "It looks like you're adding a migration, but did not update the schema file"
+      elsif migration_files.none? && schema_files.any?
+        return :bad, "You're trying to change the schema without adding a migration file"
+      elsif migration_files.any? && schema_files.any?
+        latest_version = migration_files.map  { |file| file[/\d+/] }.sort.last
+        schema         = schema_files.map     { |file| File.read(file) }.join
+        up_to_date     = schema.include?(latest_version)
+
+        unless up_to_date
+          return :bad, "The latest migration version you're committing is #{latest_version} but your schema file #{schema_files.join(' or ')} is on a different version."
+        end
+      end
+
+      :good
+    end
+
+    private
+
+    def migration_files
+      @migration_files ||= applicable_files.select do |file|
+        file.match %r{db/migrate/.*\.rb}
+      end
+    end
+
+    def schema_files
+      @schema_files    ||= applicable_files.select do |file|
+        file.match %r{db/schema\.rb|db/structure.*\.sql}
+      end
+    end
+  end
+end
