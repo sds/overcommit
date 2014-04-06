4b11b153fc8398144bdd53c7263726f56ce43a72
Jeff Felchner <accounts+git@thekompanee.com>
2014-04-06 03:03:02 -0500

    Fix grep syntax so that the output to the shell is correct

--- lib/overcommit/hook/pre_commit/pry_binding_check.rb
+++ lib/overcommit/hook/pre_commit/pry_binding_check.rb
@@ -2,7 +2,7 @@ module Overcommit::Hook::PreCommit
   # Adds a check to make sure no `binding.pry`'s have been left in the code
   class PryBindingCheck < Base
     def run
-      result = execute(%w[grep -IHn] + ['^\s*binding\.pry'] + applicable_files)
+      result = execute(%w[grep -IHnE] + ["'^\s*binding\.pry'"] + applicable_files)
 
       unless result.stdout.empty?
         return :bad, "Found a binding.pry left in:\n#{result.stdout}"
