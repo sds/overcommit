f3d4318906c102644637b1d13f5dcc92487f967d
Shane da Silva <shane@causes.com>
2014-04-17 23:12:41 -0700

    Nest class methods under class << self
    Change-Id: I2b255b449378b0b4af9b0576add1840f478a4e6a
    Reviewed-on: http://gerrit.causes.com/37392
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

--- lib/overcommit/configuration_loader.rb
+++ lib/overcommit/configuration_loader.rb
@@ -6,42 +6,44 @@ module Overcommit
     DEFAULT_CONFIG_PATH = File.join(OVERCOMMIT_HOME, 'config', 'default.yml')
     FILE_NAME = '.overcommit.yml'
 
-    def self.load_repo_config
-      overcommit_yml = File.join(Overcommit::Utils.repo_root, FILE_NAME)
+    class << self
+      def load_repo_config
+        overcommit_yml = File.join(Overcommit::Utils.repo_root, FILE_NAME)
 
-      if File.exist?(overcommit_yml)
-        load_file(overcommit_yml)
-      else
-        default_configuration
-      end
-    end
-
-    def self.default_configuration
-      @default_config ||= load_from_file(DEFAULT_CONFIG_PATH)
-    end
-
-  private
-
-    # Loads a configuration, ensuring it extends the default configuration.
-    def self.load_file(file)
-      config = load_from_file(file)
-
-      default_configuration.merge(config)
-    rescue => error
-      raise Overcommit::Exceptions::ConfigurationError,
-            "Unable to load configuration from '#{file}': #{error}",
-            error.backtrace
-    end
-
-    def self.load_from_file(file)
-      hash =
-        if yaml = YAML.load_file(file)
-          yaml.to_hash
+        if File.exist?(overcommit_yml)
+          load_file(overcommit_yml)
         else
-          {}
+          default_configuration
         end
+      end
 
-      Overcommit::Configuration.new(hash)
+      def default_configuration
+        @default_config ||= load_from_file(DEFAULT_CONFIG_PATH)
+      end
+
+    private
+
+      # Loads a configuration, ensuring it extends the default configuration.
+      def load_file(file)
+        config = load_from_file(file)
+
+        default_configuration.merge(config)
+      rescue => error
+        raise Overcommit::Exceptions::ConfigurationError,
+              "Unable to load configuration from '#{file}': #{error}",
+              error.backtrace
+      end
+
+      def load_from_file(file)
+        hash =
+          if yaml = YAML.load_file(file)
+            yaml.to_hash
+          else
+            {}
+          end
+
+        Overcommit::Configuration.new(hash)
+      end
     end
   end
 end
