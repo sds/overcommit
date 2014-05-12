7843f8024680c71f0a87bbefe84b7d0d9e72dbf8
Jeff Felchner <accounts+git@thekompanee.com>
2014-05-11 22:43:25 -0500

    Remove the 'check' from the filename as requested in the Rails Migration branch as it is redundant

--- lib/overcommit/hook/pre_commit/local_paths_in_gemfile.rb
+++ lib/overcommit/hook/pre_commit/local_paths_in_gemfile.rb
@@ -1,6 +1,6 @@
 module Overcommit::Hook::PreCommit
   # Checks for local paths in files and issues a warning
-  class LocalPathsInGemfileCheck < Base
+  class LocalPathsInGemfile < Base
     def run
       result = execute(%w[grep -IHnE (\spath:\s*)|(\s:path\s*=>)] + applicable_files)
 
