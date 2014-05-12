65744858c0285df2b9a103a344d730b3a668600c
Jeff Felchner <accounts+git@thekompanee.com>
2014-05-11 22:31:55 -0500

    Remove the 'check' from the filename as requested in the Rails Migration branch as it is redundant

--- lib/overcommit/hook/pre_commit/pry_binding.rb
+++ lib/overcommit/hook/pre_commit/pry_binding.rb
@@ -1,6 +1,6 @@
 module Overcommit::Hook::PreCommit
   # Adds a check to make sure no `binding.pry`'s have been left in the code
-  class PryBindingCheck < Base
+  class PryBinding < Base
     def run
       result = execute(%w[grep -IHnE] + ["'^\s*binding\.pry'"] + applicable_files)
 
