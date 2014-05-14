b4f37ba993b620c87f38e9056961152df51d813d
Shane da Silva <shane@causes.com>
2014-05-13 20:19:31 -0700

    Fix BerksfileCheck spec to stub in_path?
    This spec would pass locally but fail on Travis because it didn't stub
    the `in_path?` call. Fix it.
    
    Change-Id: I359f69518eae2f108761c6e48f2273a9b8cc146e
    Reviewed-on: http://gerrit.causes.com/37940
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

--- spec/overcommit/hook/pre_commit/berksfile_check_spec.rb
+++ spec/overcommit/hook/pre_commit/berksfile_check_spec.rb
@@ -13,45 +13,51 @@ describe Overcommit::Hook::PreCommit::BerksfileCheck do
     it { should warn }
   end
 
-  context 'when Berksfile.lock is ignored' do
-    around do |example|
-      repo do
-        `touch Berksfile.lock`
-        `echo Berksfile.lock > .gitignore`
-        `git add .gitignore`
-        `git commit -m "Ignore Berksfile.lock"`
-        example.run
-      end
-    end
-
-    it { should pass }
-  end
-
-  context 'when Berksfile.lock is not ignored' do
-    let(:result) { double('result') }
-
-    around do |example|
-      repo do
-        example.run
-      end
-    end
-
+  context 'when Berkshelf is installed' do
     before do
-      result.stub(:success? => success, :stderr => 'Berkshelf error message')
-      subject.stub(:execute).and_call_original
-      subject.stub(:execute).with(%w[berks list --quiet]).and_return(result)
+      subject.stub(:in_path?).and_return(true)
     end
 
-    context 'and `berks list` exits unsuccessfully' do
-      let(:success) { false }
-
-      it { should fail_hook }
-    end
-
-    context 'and `berks list` exits successfully' do
-      let(:success) { true }
+    context 'when Berksfile.lock is ignored' do
+      around do |example|
+        repo do
+          `touch Berksfile.lock`
+          `echo Berksfile.lock > .gitignore`
+          `git add .gitignore`
+          `git commit -m "Ignore Berksfile.lock"`
+          example.run
+        end
+      end
 
       it { should pass }
+    end
+
+    context 'when Berksfile.lock is not ignored' do
+      let(:result) { double('result') }
+
+      around do |example|
+        repo do
+          example.run
+        end
+      end
+
+      before do
+        result.stub(:success? => success, :stderr => 'Berkshelf error message')
+        subject.stub(:execute).and_call_original
+        subject.stub(:execute).with(%w[berks list --quiet]).and_return(result)
+      end
+
+      context 'and `berks list` exits unsuccessfully' do
+        let(:success) { false }
+
+        it { should fail_hook }
+      end
+
+      context 'and `berks list` exits successfully' do
+        let(:success) { true }
+
+        it { should pass }
+      end
     end
   end
 end
