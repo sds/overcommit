26ad8040f1e2323cb4880f0d17302ca25510d1b6
Shane da Silva <shane@causes.com>
2014-05-13 20:20:29 -0700

    Fix BundleCheck spec to stub in_path?
    This spec didn't stub the call to `in_path?` when it should have. It
    worked because `bundler` is installed on Travis, but it is better form
    to not make that assumption.
    
    Change-Id: Ie974f5e22f55c3ab977c69f232e8667a4b34460c
    Reviewed-on: http://gerrit.causes.com/37941
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

--- spec/overcommit/hook/pre_commit/bundle_check_spec.rb
+++ spec/overcommit/hook/pre_commit/bundle_check_spec.rb
@@ -13,45 +13,51 @@ describe Overcommit::Hook::PreCommit::BundleCheck do
     it { should warn }
   end
 
-  context 'when Gemfile.lock is ignored' do
-    around do |example|
-      repo do
-        `touch Gemfile.lock`
-        `echo Gemfile.lock > .gitignore`
-        `git add .gitignore`
-        `git commit -m "Ignore Gemfile.lock"`
-        example.run
-      end
-    end
-
-    it { should pass }
-  end
-
-  context 'when Gemfile.lock is not ignored' do
-    let(:result) { double('result') }
-
-    around do |example|
-      repo do
-        example.run
-      end
-    end
-
+  context 'when bundler is installed' do
     before do
-      result.stub(:success? => success, :stdout => 'Bundler error message')
-      subject.stub(:execute).and_call_original
-      subject.stub(:execute).with(%w[bundle check]).and_return(result)
+      subject.stub(:in_path?).and_return(true)
     end
 
-    context 'and bundle check exits unsuccessfully' do
-      let(:success) { false }
-
-      it { should fail_hook }
-    end
-
-    context 'and bundle check exist successfully' do
-      let(:success) { true }
+    context 'when Gemfile.lock is ignored' do
+      around do |example|
+        repo do
+          `touch Gemfile.lock`
+          `echo Gemfile.lock > .gitignore`
+          `git add .gitignore`
+          `git commit -m "Ignore Gemfile.lock"`
+          example.run
+        end
+      end
 
       it { should pass }
+    end
+
+    context 'when Gemfile.lock is not ignored' do
+      let(:result) { double('result') }
+
+      around do |example|
+        repo do
+          example.run
+        end
+      end
+
+      before do
+        result.stub(:success? => success, :stdout => 'Bundler error message')
+        subject.stub(:execute).and_call_original
+        subject.stub(:execute).with(%w[bundle check]).and_return(result)
+      end
+
+      context 'and bundle check exits unsuccessfully' do
+        let(:success) { false }
+
+        it { should fail_hook }
+      end
+
+      context 'and bundle check exist successfully' do
+        let(:success) { true }
+
+        it { should pass }
+      end
     end
   end
 end
