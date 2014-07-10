2bfa9eaa7a06334957066a4528cad70f9679353d
Shane da Silva <shane.dasilva@brigade.com>
2014-07-10 13:37:12 -0700

    Extract concerns from HookContext::PreCommit into helper class
    The `PreCommit` hook context was getting large due to it juggling a
    large number of concerns related to working with `git`.
    
    Extract these concerns into a helper `GitRepo` to make the code a little
    more straightforward.
    
    Change-Id: I7670c6e6196fcd81bfad215b92255818dd2574f8
    Reviewed-on: http://gerrit.causes.com/40383
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

---
+++ lib/overcommit/git_repo.rb
@@ -0,0 +1,121 @@
+module Overcommit
+  # Provide a set of utilities for certain interactions with `git`.
+  module GitRepo
+    module_function
+
+    # Regular expression used to extract diff ranges from hunks of diff output.
+    DIFF_HUNK_REGEX = /
+      ^@@\s
+      [^\s]+\s           # Ignore old file range
+      \+(\d+)(?:,(\d+))? # Extract range of hunk containing start line and number of lines
+      \s@@.*$
+    /x
+
+    # Extract the set of modified lines from a given file.
+    #
+    # @param file_path [String]
+    # @param options [Hash]
+    # @return [Set] line numbers that have been modified in file
+    def extract_modified_lines(file_path, options)
+      lines = Set.new
+
+      flags = '--cached' if options[:staged]
+
+      `git diff --no-ext-diff -U0 #{flags} -- #{file_path}`.
+        scan(DIFF_HUNK_REGEX) do |start_line, lines_added|
+
+        lines_added = (lines_added || 1).to_i # When blank, one line was added
+        cur_line = start_line.to_i
+
+        lines_added.times do
+          lines.add cur_line
+          cur_line += 1
+        end
+      end
+
+      lines
+    end
+
+    # Returns the names of all files that have been modified from compared to
+    # HEAD.
+    #
+    # @param options [Hash]
+    # @return [Array<String>] list of absolute file paths
+    def modified_files(options)
+      flags = '--cached' if options[:staged]
+
+      `git diff --name-only -z --diff-filter=ACM --ignore-submodules=all #{flags}`.
+        split("\0").
+        map { |relative_file| File.expand_path(relative_file) }
+    end
+
+    # Returns whether the current git branch is empty (has no commits).
+    # @return [true,false]
+    def initial_commit?
+      !Overcommit::Utils.execute(%w[git rev-parse HEAD]).success?
+    end
+
+    # Store any relevant files that are present when repo is in the middle of a
+    # merge.
+    #
+    # Restored via [#restore_merge_state].
+    def store_merge_state
+      merge_head = `git rev-parse MERGE_HEAD 2> /dev/null`.chomp
+
+      # Store the merge state if we're in the middle of resolving a merge
+      # conflict. This is necessary since stashing removes the merge state.
+      if merge_head != 'MERGE_HEAD'
+        @merge_head = merge_head
+
+        merge_msg_file = File.expand_path('.git/MERGE_MSG', Overcommit::Utils.repo_root)
+        @merge_msg = File.open(merge_msg_file).read if File.exist?(merge_msg_file)
+      end
+    end
+
+    # Store any relevant files that are present when repo is in the middle of a
+    # cherry-pick.
+    #
+    # Restored via [#restore_cherry_pick_state].
+    def store_cherry_pick_state
+      cherry_head = `git rev-parse CHERRY_PICK_HEAD 2> /dev/null`.chomp
+
+      # Store the merge state if we're in the middle of resolving a merge
+      # conflict. This is necessary since stashing removes the merge state.
+      if cherry_head != 'CHERRY_PICK_HEAD'
+        @cherry_head = cherry_head
+      end
+    end
+
+    # Restore any relevant files that were present when repo was in the middle
+    # of a merge.
+    def restore_merge_state
+      if @merge_head
+        FileUtils.touch(File.expand_path('.git/MERGE_MODE', Overcommit::Utils.repo_root))
+
+        File.open(File.expand_path('.git/MERGE_HEAD', Overcommit::Utils.repo_root), 'w') do |f|
+          f.write("#{@merge_head}\n")
+        end
+        @merge_head = nil
+      end
+
+      if @merge_msg
+        File.open(File.expand_path('.git/MERGE_MSG', Overcommit::Utils.repo_root), 'w') do |f|
+          f.write("#{@merge_msg}\n")
+        end
+        @merge_msg = nil
+      end
+    end
+
+    # Restore any relevant files that were present when repo was in the middle
+    # of a cherry-pick.
+    def restore_cherry_pick_state
+      if @cherry_head
+        File.open(File.expand_path('.git/CHERRY_PICK_HEAD',
+                                   Overcommit::Utils.repo_root), 'w') do |f|
+          f.write("#{@cherry_head}\n")
+        end
+        @cherry_head = nil
+      end
+    end
+  end
+end
--- lib/overcommit/hook_context/pre_commit.rb
+++ lib/overcommit/hook_context/pre_commit.rb
@@ -5,14 +5,15 @@ module Overcommit::HookContext
   # Contains helpers related to contextual information used by pre-commit hooks.
   #
   # This includes staged files, which lines of those files have been modified,
-  # etc.
+  # etc. It is also responsible for saving/restoring the state of the repo so
+  # hooks only inspect staged changes.
   class PreCommit < Base
     # Stash unstaged contents of files so hooks don't see changes that aren't
     # about to be committed.
     def setup_environment
       store_modified_times
-      store_merge_state
-      store_cherry_pick_state
+      Overcommit::GitRepo.store_merge_state
+      Overcommit::GitRepo.store_cherry_pick_state
 
       if !initial_commit? && any_changes?
         @changes_stashed = true
@@ -36,25 +37,22 @@ module Overcommit::HookContext
         `git stash pop --index --quiet`
       end
 
-      restore_merge_state
-      restore_cherry_pick_state
+      Overcommit::GitRepo.restore_merge_state
+      Overcommit::GitRepo.restore_cherry_pick_state
       restore_modified_times
     end
 
     # Get a list of added, copied, or modified files that have been staged.
     # Renames and deletions are ignored, since there should be nothing to check.
     def modified_files
-      @modified_files ||=
-        `git diff --cached --name-only -z --diff-filter=ACM --ignore-submodules=all`.
-          split("\0").
-          map { |relative_file| File.expand_path(relative_file) }
+      @modified_files ||= Overcommit::GitRepo.modified_files(:staged => true)
     end
 
     # Returns the set of line numbers corresponding to the lines that were
     # changed in a specified file.
     def modified_lines(file)
       @modified_lines ||= {}
-      @modified_lines[file] ||= extract_modified_lines(file)
+      @modified_lines[file] ||= Overcommit::GitRepo.extract_modified_lines(file, :staged => true)
     end
 
   private
@@ -72,85 +70,14 @@ module Overcommit::HookContext
     # Returns whether the current git branch is empty (has no commits).
     def initial_commit?
       return @initial_commit unless @initial_commit.nil?
-      @initial_commit = !Overcommit::Utils.execute(%w[git rev-parse HEAD]).success?
+      @initial_commit = Overcommit::GitRepo.initial_commit?
     end
 
-    DIFF_HUNK_REGEX = /
-      ^@@\s
-      [^\s]+\s           # Ignore old file range
-      \+(\d+)(?:,(\d+))? # Extract range of hunk containing start line and number of lines
-      \s@@.*$
-    /x
-
-    def extract_modified_lines(staged_file)
-      lines = Set.new
-
-      `git diff --no-ext-diff --cached -U0 -- #{staged_file}`.
-        scan(DIFF_HUNK_REGEX) do |start_line, lines_added|
-
-        lines_added = (lines_added || 1).to_i # When blank, one line was added
-        cur_line = start_line.to_i
-
-        lines_added.times do
-          lines.add cur_line
-          cur_line += 1
-        end
-      end
-
-      lines
-    end
-
-    def store_merge_state
-      merge_head = `git rev-parse MERGE_HEAD 2> /dev/null`.chomp
-
-      # Store the merge state if we're in the middle of resolving a merge
-      # conflict. This is necessary since stashing removes the merge state.
-      if merge_head != 'MERGE_HEAD'
-        @merge_head = merge_head
-
-        merge_msg_file = File.expand_path('.git/MERGE_MSG', Overcommit::Utils.repo_root)
-        @merge_msg = File.open(merge_msg_file).read if File.exist?(merge_msg_file)
-      end
-    end
-
-    def store_cherry_pick_state
-      cherry_head = `git rev-parse CHERRY_PICK_HEAD 2> /dev/null`.chomp
-
-      # Store the merge state if we're in the middle of resolving a merge
-      # conflict. This is necessary since stashing removes the merge state.
-      if cherry_head != 'CHERRY_PICK_HEAD'
-        @cherry_head = cherry_head
-      end
-    end
-
-    def restore_merge_state
-      if @merge_head
-        FileUtils.touch(File.expand_path('.git/MERGE_MODE', Overcommit::Utils.repo_root))
-
-        File.open(File.expand_path('.git/MERGE_HEAD', Overcommit::Utils.repo_root), 'w') do |f|
-          f.write("#{@merge_head}\n")
-        end
-        @merge_head = nil
-      end
-
-      if @merge_msg
-        File.open(File.expand_path('.git/MERGE_MSG', Overcommit::Utils.repo_root), 'w') do |f|
-          f.write("#{@merge_msg}\n")
-        end
-        @merge_msg = nil
-      end
-    end
-
-    def restore_cherry_pick_state
-      if @cherry_head
-        File.open(File.expand_path('.git/CHERRY_PICK_HEAD',
-                                   Overcommit::Utils.repo_root), 'w') do |f|
-          f.write("#{@cherry_head}\n")
-        end
-        @cherry_head = nil
-      end
-    end
-
+    # Stores the modification times for all modified files to make it appear like
+    # they never changed.
+    #
+    # This prevents (some) editors from complaining about files changing when we
+    # stash changes before running the hooks.
     def store_modified_times
       @modified_times = {}
 
@@ -159,11 +86,8 @@ module Overcommit::HookContext
       end
     end
 
-    # Stores the modification times for all modified files to make it appear like
-    # they never changed.
-    #
-    # This prevents editors from complaining about files changing when we stash
-    # changes before running the hooks.
+    # Restores the file modification times for all modified files to make it
+    # appear like they never changed.
     def restore_modified_times
       modified_files.each do |file|
         time = @modified_times[file]
