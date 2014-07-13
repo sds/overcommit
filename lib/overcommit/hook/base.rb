sds/overcommit

commit 0e3540d0c86f2447bd89c9c65ebcb7880c97085b

Fix handling of shell globbing
Apparently `File.fnmatch` doesn't have the same behavior as shell
globbing when using the `**` pattern. This results in strange behavior
like `**/*.rb` only matching Ruby files nested under at least one
directory.

Fix this by using `Dir[glob].include?(file)` instead of
`File.fnmatch(glob, file)`, to match the behavior of shell globbing.

This also allows us to remove a hack in `Hook::Base` which handles this
case for files in the root directory.

Change-Id: I3fd26420e236d1c5e18506752fedf9f533d14d42
Reviewed-on: http://gerrit.causes.com/37981
Tested-by: jenkins <jenkins@causes.com>
Reviewed-by: Shane da Silva <shane@causes.com>

lib/overcommit/hook/base.rb
@@ -70,22 +70,17 @@ module Overcommit::Hook
     def applicable_file?(file)
       includes = Array(@config['include']).map { |glob| convert_glob_to_absolute(glob) }
       included = includes.empty? ||
-                 includes.any? { |glob| File.fnmatch(glob, file) }
+                 includes.any? { |glob| Dir[glob].include?(file) }
 
       excludes = Array(@config['exclude']).map { |glob| convert_glob_to_absolute(glob) }
-      excluded = excludes.any? { |glob| File.fnmatch(glob, file) }
+      excluded = excludes.any? { |glob| Dir[glob].include?(file) }
 
       included && !excluded
     end
 
     def convert_glob_to_absolute(glob)
       repo_root = Overcommit::Utils.repo_root
-
-      if glob.start_with?('**')
-        repo_root + glob # Want ** to match items in the repo root as well
-      else
-        File.join(repo_root, glob)
-      end
+      File.join(repo_root, glob)
     end
   end
 end

lib/overcommit/hook/post_checkout/bundle_check.rb
@@ -22,7 +22,7 @@ module Overcommit::Hook::PostCheckout
       result = execute(%w[git diff --exit-code --name-only] + [new_head, previous_head])
 
       result.stdout.split("\n").any? do |file|
-        Array(@config['include']).any? { |glob| File.fnmatch(glob, file) }
+        Array(@config['include']).any? { |glob| Dir[glob].include?(file) }
       end
     end
 

commit 1b7c5e50f5d0110dde8639eafc79b8736ab7f1da

Fix performance regression in file include/exclude logic
Commit 0e3540d introduced a performance regression where running
Overcommit in repositories with a large number of files that could
potentially apply to a given hook (even if they weren't being staged)
would result in a very slow hook run.

This was due to us switching from `File.fnmatch?` to `Dir.glob` because
at the time the behavior of the latter was what we were trying to
achieve. However, this approach also scans the actual filesystem, making
it slow.

Further investigation revealed that `File.fnmatch?` can take an
additional flag that changes the behavior to act more like `Dir.glob`,
namely resulting in `**/*.rb` matching all Ruby files, both at the root
as well as in subdirectories.

Change-Id: I1e4e66e8aa50d72381301762d5642133515a0b85
Reviewed-on: http://gerrit.causes.com/40419
Tested-by: jenkins <jenkins@causes.com>
Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

lib/overcommit/hook/base.rb
@@ -70,10 +70,10 @@ module Overcommit::Hook
     def applicable_file?(file)
       includes = Array(@config['include']).map { |glob| convert_glob_to_absolute(glob) }
       included = includes.empty? ||
-                 includes.any? { |glob| Dir[glob].include?(file) }
+                 includes.any? { |glob| matches_path?(glob, file) }
 
       excludes = Array(@config['exclude']).map { |glob| convert_glob_to_absolute(glob) }
-      excluded = excludes.any? { |glob| Dir[glob].include?(file) }
+      excluded = excludes.any? { |glob| matches_path?(glob, file) }
 
       included && !excluded
     end
@@ -81,6 +81,14 @@ module Overcommit::Hook
     def convert_glob_to_absolute(glob)
       repo_root = Overcommit::Utils.repo_root
       File.join(repo_root, glob)
+    end
+
+    # Return whether a pattern matches the given path.
+    #
+    # @param pattern [String]
+    # @param path [String]
+    def matches_path?(pattern, path)
+      File.fnmatch?(pattern, path, File::FNM_PATHNAME)
     end
   end
 end