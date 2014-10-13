fc3acf74c91bc80a82b2c86a10bd7dc96e6aa387
Alex Buijs <abuijs@digidentity.com>
2014-10-13 15:49:18 +0200

    Pass comma separated list of files to brakeman
    When a space separated list of files is passed to brakeman with the
    `--only-files` option, brakeman fails with the error 'Please supply the
    path to a Rails application.' When the files are passed as a comma
    separated string, all is well.
    
    Change-Id: I52f998b395d9393b646b1896817e3905c12dfbec
    Reviewed-on: http://gerrit.causes.com/43692
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/overcommit/hook/pre_commit/brakeman.rb
+++ lib/overcommit/hook/pre_commit/brakeman.rb
@@ -2,8 +2,8 @@ module Overcommit::Hook::PreCommit
   # Runs `brakeman` against any modified Ruby/Rails files.
   class Brakeman < Base
     def run
-      result = execute(%W[#{executable} --exit-on-warn --quiet --summary --only-files] +
-                       applicable_files)
+      result = execute(%W[#{executable} --exit-on-warn --quiet --summary --only-files] <<
+                       applicable_files.join(','))
       return :pass if result.success?
 
       [:fail, result.stdout]
