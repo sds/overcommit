20a99612e8126c88892c713736ad85abaab1a618
Shane da Silva <shane@dasilva.io>
2014-11-18 22:17:23 -0800

    Clean up RunAll context
    This had unnecessary `require` statements and extraneous documentation.
    
    Change-Id: If7882c999ac6d8a874ca12986f5341dd408b9b60
    Reviewed-on: http://gerrit.causes.com/44657
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/overcommit/hook_context/run_all.rb
+++ lib/overcommit/hook_context/run_all.rb
@@ -1,30 +1,21 @@
-require 'fileutils'
-require 'set'
-
 module Overcommit::HookContext
-  # Contains helpers related to contextual information used by pre-commit hooks.
+  # Simulates a pre-commit context pretending that all files have been changed.
   #
-  # This includes staged files, which lines of those files have been modified,
-  # etc. It is also responsible for saving/restoring the state of the repo so
-  # hooks only inspect staged changes.
+  # This results in pre-commit hooks running against the entire repository,
+  # which is useful for automated CI scripts.
   class RunAll < Base
-    # Get a list of added, copied, or modified files that have been staged.
-    # Renames and deletions are ignored, since there should be nothing to check.
     def modified_files
       all_files
     end
 
-    # Returns the camel-cased type of this hook (e.g. PreCommit)
     def hook_class_name
       'PreCommit'
     end
 
-    # Returns the snake-cased type of this hook (e.g. pre_commit)
     def hook_type_name
       'pre_commit'
     end
 
-    # Returns the actual name of the hook script being run (e.g. pre-commit).
     def hook_script_name
       'pre-commit'
     end
