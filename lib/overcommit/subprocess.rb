dbae7f79912dd3ad992e3046404450b2e8b824c6
Shane da Silva <shane@dasilva.io>
2014-11-30 14:20:53 +0700

    DRY up Subprocess.spawn
    This was recommended by RuboCop as a means of reducing the number of
    statements in the method. I'm not really convinced this was worth it,
    but it's an experiment for now.
    
    Change-Id: I88b8e7adb5539650fe490b889f2809fff1d57146
    Reviewed-on: http://gerrit.causes.com/44894
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/overcommit/subprocess.rb
+++ lib/overcommit/subprocess.rb
@@ -6,32 +6,41 @@ module Overcommit
   # standard out/error output.
   class Subprocess
     # Encapsulates the result of a process.
-    Result = Struct.new(:status, :stderr, :stdout) do
+    Result = Struct.new(:status, :stdout, :stderr) do
       def success?
         status == 0
       end
     end
 
-    # Spawns a new process using the given array of arguments (the first
-    # element is the command).
-    def self.spawn(args)
-      process = ChildProcess.build(*args)
+    class << self
+      # Spawns a new process using the given array of arguments (the first
+      # element is the command).
+      def spawn(args)
+        process = ChildProcess.build(*args)
 
-      err = ::Tempfile.new('err')
-      err.sync = true
-      out = ::Tempfile.new('out')
-      out.sync = true
+        out, err = assign_output_streams(process)
 
-      process.io.stderr = err
-      process.io.stdout = out
+        process.start
+        process.wait
 
-      process.start
-      process.wait
+        err.rewind
+        out.rewind
 
-      err.rewind
-      out.rewind
+        Result.new(process.exit_code, out.read, err.read)
+      end
 
-      Result.new(process.exit_code, err.read, out.read)
+      private
+
+      # @param process [ChildProcess]
+      # @return [Array<IO>]
+      def assign_output_streams(process)
+        %w[out err].map do |stream_name|
+          ::Tempfile.new(stream_name).tap do |stream|
+            stream.sync = true
+            process.io.send("std#{stream_name}=", stream)
+          end
+        end
+      end
     end
   end
 end
