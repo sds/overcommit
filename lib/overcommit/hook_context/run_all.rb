cd43493b2a15ced9936148d7e37e48572ea28440
Shane da Silva <shane@dasilva.io>
2014-11-30 16:00:39 +0700

    Fix RunAll context to define modified_lines_in_file
    Change-Id: Ic3553043bff52a5a2893b41813f866e88ca75752
    Reviewed-on: http://gerrit.causes.com/44906
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/overcommit/hook_context/run_all.rb
+++ lib/overcommit/hook_context/run_all.rb
@@ -1,11 +1,21 @@
+require 'set'
+
 module Overcommit::HookContext
   # Simulates a pre-commit context pretending that all files have been changed.
   #
   # This results in pre-commit hooks running against the entire repository,
   # which is useful for automated CI scripts.
   class RunAll < Base
+    EMPTY_SET = Set.new
+
     def modified_files
       all_files
+    end
+
+    # Return an empty set since in this context the user didn't actually touch
+    # any lines.
+    def modified_lines_in_file(_file)
+      EMPTY_SET
     end
 
     def hook_class_name
