fea861bc4720cc503867b76d9263c59713b7428a
Shane da Silva <shane@dasilva.io>
2014-11-30 14:20:46 +0700

    Document Logger class
    Add some simple documentation for this utility class.
    
    Change-Id: Ifc6bd694d99c7cb282c067925550d9ca2fb859ae
    Reviewed-on: http://gerrit.causes.com/44892
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/overcommit/logger.rb
+++ lib/overcommit/logger.rb
@@ -6,10 +6,14 @@ module Overcommit
       new(File.open('/dev/null', 'w'))
     end
 
+    # Creates a logger that will write to the given output stream.
+    #
+    # @param out [IO]
     def initialize(out)
       @out = out
     end
 
+    # Write output without a trailing newline.
     def partial(*args)
       @out.print(*args)
     end
@@ -19,36 +23,54 @@ module Overcommit
       log
     end
 
+    # Write a line of output.
+    #
+    # A newline character will always be appended.
     def log(*args)
       @out.puts(*args)
     end
 
+    # Write a line of output that is intended to be emphasized.
     def bold(*args)
       color('1', *args)
     end
 
+    # Write a line of output indicating a problem or error.
     def error(*args)
       color(31, *args)
     end
 
+    # Write a line of output indicating a problem or error which is emphasized
+    # over a regular problem or error.
     def bold_error(*args)
       color('1;31', *args)
     end
 
+    # Write a line of output indicating a successful or noteworthy event.
     def success(*args)
       color(32, *args)
     end
 
+    # Write a line of output indicating a potential cause for concern, but not
+    # an actual error.
     def warning(*args)
       color(33, *args)
     end
 
+    # Write a line of output indicating a potential cause for concern, but with
+    # greater emphasize compared to other warnings.
     def bold_warning(*args)
       color('1;33', *args)
     end
 
     private
 
+    # Outputs text wrapped in ANSI escape code necessary to produce a given
+    # color/text display.
+    #
+    # @param code [String] ANSI escape code, e.g. '1;33' for "bold yellow"
+    # @param str [String] string to wrap
+    # @param partial [true,false] whether to omit a newline
     def color(code, str, partial = false)
       send(partial ? :partial : :log,
            @out.tty? ? "\033[#{code}m#{str}\033[0m" : str)
