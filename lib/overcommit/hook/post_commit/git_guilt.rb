e2cf92d394f793a48787619d61573876ca9bb9a0
Josh Hagins <hagins.josh@gmail.com>
2015-02-18 18:39:27 -0500

    Use required_executable instead of executable

--- lib/overcommit/hook/post_commit/git_guilt.rb
+++ lib/overcommit/hook/post_commit/git_guilt.rb
@@ -2,7 +2,7 @@ module Overcommit::Hook::PostCommit
   # Calculates the change in blame since the last revision.
   class GitGuilt < Base
     def run
-      result = execute([executable, 'HEAD~', 'HEAD'])
+      result = execute([required_executable, 'HEAD~', 'HEAD'])
       puts result.stdout
       :pass
     end
