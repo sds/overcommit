d6f66ade81049f8b87b9950aeadf6f262304509a
Josh Hagins <hagins.josh@gmail.com>
2015-02-18 16:38:47 -0500

    Add tests for `Overcommit::HookContext::PostCommit`
    Change-Id: I05ddef5679719d4814c0deb4a77c2bf1a64c431b
    Reviewed-on: http://gerrit.causes.com/46926
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>
    Tested-by: Shane da Silva <shane.dasilva@brigade.com>

---
+++ spec/overcommit/hook_context/post_commit_spec.rb
@@ -0,0 +1,116 @@
+require 'spec_helper'
+require 'overcommit/hook_context/post_commit'
+
+describe Overcommit::HookContext::PostCommit do
+  let(:config) { double('config') }
+  let(:args) { [] }
+  let(:context) { described_class.new(config, args) }
+
+  describe '#modified_files' do
+    subject { context.modified_files }
+
+    it 'does not include submodules' do
+      submodule = repo do
+        `touch foo`
+        `git add foo`
+        `git commit -m "Initial commit"`
+      end
+
+      repo do
+        `git submodule add #{submodule} test-sub 2>&1 > /dev/null`
+        `git commit -m "Initial commit"`
+        expect(subject).to_not include File.expand_path('test-sub')
+      end
+    end
+
+    context 'when no files were staged' do
+      around do |example|
+        repo do
+          `git commit --allow-empty -m "Initial commit"`
+          example.run
+        end
+      end
+
+      it { should be_empty }
+    end
+
+    context 'when files were added' do
+      around do |example|
+        repo do
+          FileUtils.touch('some-file')
+          `git add some-file`
+          `git commit -m "Initial commit"`
+          example.run
+        end
+      end
+
+      it { should == [File.expand_path('some-file')] }
+    end
+
+    context 'when files were modified' do
+      around do |example|
+        repo do
+          FileUtils.touch('some-file')
+          `git add some-file`
+          `git commit -m 'Initial commit'`
+          `echo Hello > some-file`
+          `git add some-file`
+          `git commit -m 'Modify some-file'`
+          example.run
+        end
+      end
+
+      it { should == [File.expand_path('some-file')] }
+    end
+
+    context 'when files were deleted' do
+      around do |example|
+        repo do
+          FileUtils.touch('some-file')
+          `git add some-file`
+          `git commit -m 'Initial commit'`
+          `git rm some-file`
+          `git commit -m 'Delete some-file'`
+          example.run
+        end
+      end
+
+      it { should be_empty }
+    end
+  end
+
+  describe '#modified_lines_in_file' do
+    let(:modified_file) { 'some-file' }
+    subject { context.modified_lines_in_file(modified_file) }
+
+    context 'when file contains a trailing newline' do
+      around do |example|
+        repo do
+          File.open(modified_file, 'w') { |f| (1..3).each { |i| f.write("#{i}\n") } }
+          `git add #{modified_file}`
+          `git commit -m "Add files"`
+          example.run
+        end
+      end
+
+      it { should == Set.new(1..3) }
+    end
+
+    context 'when file does not contain a trailing newline' do
+      around do |example|
+        repo do
+          File.open(modified_file, 'w') do |f|
+            (1..2).each { |i| f.write("#{i}\n") }
+            f.write(3)
+          end
+
+          `git add #{modified_file}`
+          `git commit -m "Add files"`
+          example.run
+        end
+      end
+
+      it { should == Set.new(1..3) }
+    end
+  end
+end
