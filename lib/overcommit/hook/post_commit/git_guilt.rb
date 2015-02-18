e86dc4791aed79a88ae1655b3505d9f5ff6f9584
Josh Hagins <hagins.josh@gmail.com>
2015-02-18 18:39:59 -0500

    Don't run git-guilt after initial commit

--- lib/overcommit/hook/post_commit/git_guilt.rb
+++ lib/overcommit/hook/post_commit/git_guilt.rb
@@ -2,6 +2,7 @@ module Overcommit::Hook::PostCommit
   # Calculates the change in blame since the last revision.
   class GitGuilt < Base
     def run
+      return :pass unless previous_commit?
       result = execute([required_executable, 'HEAD~', 'HEAD'])
       puts result.stdout
       :pass
