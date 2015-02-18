5d731d7f45373e1549147214a7e5ca87c4d1799c
Josh Hagins <hagins.josh@gmail.com>
2015-02-18 16:36:58 -0500

    Use `git show --format=` rather than `git diff` for post-commit
    `git diff HEAD~ HEAD` fails if there is no previous commit.
    
    Change-Id: Ib2fcba689575dc599c08df33f1e212e5a111bb65
    Reviewed-on: http://gerrit.causes.com/46925
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/overcommit/hook_context/post_commit.rb
+++ lib/overcommit/hook_context/post_commit.rb
@@ -6,15 +6,26 @@ module Overcommit::HookContext
     # commit. Renames and deletions are ignored, since there should be nothing
     # to check.
     def modified_files
-      @modified_files ||= Overcommit::GitRepo.modified_files(refs: 'HEAD~ HEAD')
+      subcmd = 'show --format='
+      @modified_files ||= Overcommit::GitRepo.modified_files(subcmd: subcmd)
     end
 
     # Returns the set of line numbers corresponding to the lines that were
     # changed in a specified file.
     def modified_lines_in_file(file)
+      subcmd = 'show --format='
       @modified_lines ||= {}
       @modified_lines[file] ||=
-        Overcommit::GitRepo.extract_modified_lines(file, refs: 'HEAD~ HEAD')
+        Overcommit::GitRepo.extract_modified_lines(file, subcmd: subcmd)
+    end
+
+    private
+
+    # Returns whether a previous commit exists on the current git branch.
+    # @return [true,false]
+    def previous_commit?
+      return @previous_commit unless @previous_commit.nil?
+      @previous_commit = Overcommit::Utils.execute(%w[git rev-parse HEAD~]).success?
     end
   end
 end
