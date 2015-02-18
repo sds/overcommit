6a3aaeec08df7e21f72f09c1cae72f750369c472
Josh Hagins <hagins.josh@gmail.com>
2015-02-18 17:44:00 -0500

    Use execute helper method

--- lib/overcommit/hook/post_commit/git_guilt.rb
+++ lib/overcommit/hook/post_commit/git_guilt.rb
@@ -2,7 +2,8 @@ module Overcommit::Hook::PostCommit
   # Calculates the change in blame since the last revision.
   class GitGuilt < Base
     def run
-      system(executable, 'HEAD~', 'HEAD')
+      result = execute([executable, 'HEAD~', 'HEAD'])
+      puts result.stdout
       :pass
     end
   end
