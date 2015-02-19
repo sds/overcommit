2842072e3e77b65ad17341e775417d47d3046ef0
Josh Hagins <hagins.josh@gmail.com>
2015-02-18 19:40:18 -0500

    Use initial_commit?

--- lib/overcommit/hook/post_commit/git_guilt.rb
+++ lib/overcommit/hook/post_commit/git_guilt.rb
@@ -2,7 +2,7 @@ module Overcommit::Hook::PostCommit
   # Calculates the change in blame since the last revision.
   class GitGuilt < Base
     def run
-      return :pass unless previous_commit?
+      return :pass if initial_commit?
       result = execute(command)
       puts result.stdout
       :pass
