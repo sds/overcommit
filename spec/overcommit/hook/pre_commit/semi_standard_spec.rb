175059fa7adf4830373388bee63a934299ae7493
Josh Hagins <hagins.josh@gmail.com>
2015-02-19 13:54:26 -0500

    Add semistandard pre-commit hook
    Change-Id: If97be868b021291dd5bf79363308be6aedd1b4ef
    Reviewed-on: http://gerrit.causes.com/46845
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Joe Lencioni <joe.lencioni@brigade.com>

---
+++ spec/overcommit/hook/pre_commit/semi_standard_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::SemiStandard do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+
+  before do
+    subject.stub(:applicable_files).and_return(%w[file1.js file2.js])
+  end
+
+  context 'when semistandard exits successfully' do
+    before do
+      result = double('result')
+      result.stub(:success? => true, :stderr => '')
+      subject.stub(:execute).and_return(result)
+    end
+
+    it { should pass }
+  end
+
+  context 'when semistandard exits unsucessfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(false)
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'and it reports an error' do
+      before do
+        result.stub(:stderr).and_return([
+          'Error: Code style check failed:',
+          'file1.js:1:1: Extra semicolon. (eslint/semi)'
+        ].join("\n"))
+
+        subject.stub(:modified_lines_in_file).and_return([1, 2])
+      end
+
+      it { should fail_hook }
+    end
+  end
+end
