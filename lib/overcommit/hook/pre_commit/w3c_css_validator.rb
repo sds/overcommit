4a95f9790e49c5b2a412fc71352185804f35e365
Josh Hagins <hagins.josh@gmail.com>
2015-02-25 03:18:01 -0500

    Add pre-commit hooks for w3c_validators
    Change-Id: I0570cc7cca4f16c965ec45f6c8c7a687a57a206a
    Reviewed-on: http://gerrit.causes.com/47048
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>
    Tested-by: Shane da Silva <shane.dasilva@brigade.com>

---
+++ lib/overcommit/hook/pre_commit/w3c_css_validator.rb
@@ -0,0 +1,74 @@
+module Overcommit::Hook::PreCommit
+  # Runs `w3c_validators` against any modified CSS files.
+  class W3cCssValidator < Base
+    def run
+      begin
+        require 'w3c_validators'
+      rescue LoadError
+        return :fail, 'w3c_validators not installed -- run `gem install w3c_validators`'
+      end
+
+      result_messages =
+        begin
+          applicable_files.collect do |path|
+            results = validator.validate_file(path)
+            messages = results.errors + results.warnings
+            messages.collect do |msg|
+              # Some warnings are not per-line, so use 0 as a default
+              line = Integer(msg.line || 0)
+
+              # Build message by hand to reduce noise from the validator response
+              text = "#{msg.type.to_s.upcase}; URI: #{path}; line #{line}: #{msg.message.strip}"
+              Overcommit::Hook::Message.new(msg.type, path, line, text)
+            end
+          end
+        rescue W3CValidators::ValidatorUnavailable => e
+          return :fail, e.message
+        rescue W3CValidators::ParsingError => e
+          return :fail, e.message
+        end
+
+      result_messages.flatten!
+      return :pass if result_messages.empty?
+
+      result_messages
+    end
+
+    private
+
+    def validator
+      unless @validator
+        @validator = W3CValidators::CSSValidator.new(opts)
+        @validator.set_language!(language) unless language.nil?
+        @validator.set_profile!(profile) unless profile.nil?
+        @validator.set_warn_level!(warn_level) unless warn_level.nil?
+      end
+      @validator
+    end
+
+    def opts
+      @opts ||= {
+        validator_uri: config['validator_uri'],
+        proxy_server:  config['proxy_server'],
+        proxy_port:    config['proxy_port'],
+        proxy_user:    config['proxy_user'],
+        proxy_pass:    config['proxy_pass']
+      }
+    end
+
+    def language
+      @language ||= config['language']
+    end
+
+    # Values specified at
+    #   http://www.rubydoc.info/gems/w3c_validators/1.2/W3CValidators#CSS_PROFILES
+    def profile
+      @profile ||= config['profile']
+    end
+
+    # One of 0, 1, 2, 'no'
+    def warn_level
+      @warn_level ||= config['warn_level']
+    end
+  end
+end
