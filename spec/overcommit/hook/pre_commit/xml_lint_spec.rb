ca5ea7c944df6a2a420120d240859aa6991755b7
Josh Hagins <hagins.josh@gmail.com>
2015-02-25 23:29:41 -0500

    Add pre-commit hook for xmllint
    Change-Id: Ic7b832a92dc0a1a6ef83d29b3b63436420551244
    Reviewed-on: http://gerrit.causes.com/47089
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

---
+++ spec/overcommit/hook/pre_commit/xml_lint_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::XmlLint do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+
+  before do
+    subject.stub(:applicable_files).and_return(%w[file1.xml file2.xml])
+  end
+
+  context 'when xmllint exits successfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(true)
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'with no errors' do
+      before do
+        result.stub(:stderr).and_return('')
+      end
+
+      it { should pass }
+    end
+  end
+
+  context 'when xmllint exits unsucessfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(false)
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'and it reports an error' do
+      before do
+        result.stub(:stderr).and_return([
+          "file1.xml:1: parser error : expected '='"
+        ].join("\n"))
+
+        subject.stub(:modified_lines_in_file).and_return([1, 2])
+      end
+
+      it { should fail_hook }
+    end
+  end
+end
