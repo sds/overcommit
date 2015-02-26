a0a940520ab897027285ee064430056e2a159eb5
Josh Hagins <hagins.josh@gmail.com>
2015-02-26 05:03:05 -0500

    Add pre-commit hook for tidy
    Add hook for tidy, an HTML linter.
    
    Change-Id: I8909622886bf79fe3ad25549e6f9d6b9096f8e03

---
+++ lib/overcommit/hook/pre_commit/html_tidy.rb
@@ -0,0 +1,35 @@
+module Overcommit::Hook::PreCommit
+  # Runs `tidy` against any modified HTML files.
+  class HtmlTidy < Base
+    MESSAGE_REGEX = /
+      ^(?<file>[^:]+):\s
+      line\s(?<line>\d+)\s
+      column\s(?<col>\d+)\s-\s
+      (?<type>Error|Warning):\s(?<message>.+)$
+    /x
+
+    def run
+      # example message:
+      #   line 4 column 24 - Warning: <html> proprietary attribute "class"
+      messages = collect_messages
+
+      return :pass if messages.empty?
+
+      messages
+    end
+
+    private
+
+    def collect_messages
+      applicable_files.collect do |file|
+        result = execute(command + [file])
+        output = result.stderr.chomp
+        extract_messages(
+          output.split("\n").collect { |msg| "#{file}: #{msg}" },
+          MESSAGE_REGEX,
+          lambda { |type| type.downcase.to_sym }
+        )
+      end.flatten
+    end
+  end
+end
