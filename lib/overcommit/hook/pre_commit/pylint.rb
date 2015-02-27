6f42339307b652f1e46bf10a2c2e2a5ae1f448d1
Josh Hagins <hagins.josh@gmail.com>
2015-02-26 20:25:31 -0500

    Add pre-commit hook for Pylint
    Change-Id: Iac45e695f4ba29db43a421ce2732ca9378aee104

---
+++ lib/overcommit/hook/pre_commit/pylint.rb
@@ -0,0 +1,28 @@
+module Overcommit::Hook::PreCommit
+  # Runs `pylint` against any modified Python files.
+  class Pylint < Base
+    MESSAGE_REGEX = /^(?<file>.+):(?<line>\d+):(?<type>[CEFRW])/
+
+    # Classify 'E' and 'F' message codes as errors,
+    # everything else as warnings.
+    #   http://pylint.readthedocs.org/en/latest/tutorial.html#getting-started
+    MESSAGE_TYPE_CATEGORIZER = lambda do |type|
+      'EF'.include?(type) ? :error : :warning
+    end
+
+    def run
+      result = execute(command + applicable_files)
+      return :pass if result.success?
+
+      output = result.stdout.chomp
+
+      # example message:
+      #   path/to/file.py:64:C: Missing function docstring (missing-docstring)
+      extract_messages(
+        output.split("\n").grep(MESSAGE_REGEX),
+        MESSAGE_REGEX,
+        MESSAGE_TYPE_CATEGORIZER
+      )
+    end
+  end
+end
