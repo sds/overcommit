5a8e7ddf8c5a7b102d2a47460bc0cff84c6afe6f
Josh Hagins <hagins.josh@gmail.com>
2015-02-27 13:54:56 -0500

    Parse errors and warnings in CssLint output
    Change-Id: Iab4500b6208c69df10e5fe52902ab29940f82b5b

--- spec/overcommit/hook/pre_commit/css_lint_spec.rb
+++ spec/overcommit/hook/pre_commit/css_lint_spec.rb
@@ -9,23 +9,73 @@ describe Overcommit::Hook::PreCommit::CssLint do
     subject.stub(:applicable_files).and_return(%w[file1.css file2.css])
   end
 
-  context 'when csslint exits with no output' do
+  context 'when csslint exits successfully' do
+    let(:result) { double('result') }
+
     before do
-      result = double('result')
-      result.stub(:stdout).and_return('')
+      result.stub(:success?).and_return(true)
       subject.stub(:execute).and_return(result)
     end
 
-    it { should pass }
+    context 'with no output' do
+      before do
+        result.stub(:stdout).and_return('')
+      end
+
+      it { should pass }
+    end
+
+    context 'and it reports a warning' do
+      context 'with a line number' do
+        before do
+          result.stub(:stdout).and_return([
+            'file1.css: line 1, col 5, Warning - Use of !important'
+          ].join("\n"))
+        end
+
+        it { should warn }
+      end
+
+      context 'with no line number' do
+        before do
+          result.stub(:stdout).and_return([
+            'file1.css: Warning - Too many !important declarations (10), try to use less than 10'
+          ].join("\n"))
+        end
+
+        it { should warn }
+      end
+    end
   end
 
-  context 'when csslint exits with output' do
+  context 'when csslint exits unsucessfully' do
+    let(:result) { double('result') }
+
     before do
-      result = double('result')
-      result.stub(:stdout).and_return('Error - @charset not allowed here')
+      result.stub(:success?).and_return(false)
       subject.stub(:execute).and_return(result)
     end
 
-    it { should fail_hook }
+    context 'and it reports an error' do
+      context 'with a line number' do
+        before do
+          result.stub(:stdout).and_return([
+            'file1.css: line 80, col 5, Error - Use of !important'
+          ].join("\n"))
+        end
+
+        it { should fail_hook }
+      end
+
+      context 'with no line number' do
+        before do
+          result.stub(:stdout).and_return([
+            'file1.css: Error - Currently no rules report a rollup error, but that may change'
+          ].join("\n"))
+        end
+
+        it { should fail_hook }
+      end
+    end
   end
 end
