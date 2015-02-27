6583e013eea5cff78119028e9e3285a4696a02ef
Josh Hagins <hagins.josh@gmail.com>
2015-02-26 20:25:31 -0500

    Add pre-commit hook for Pylint
    Change-Id: Iac45e695f4ba29db43a421ce2732ca9378aee104
    Reviewed-on: http://gerrit.causes.com/47136
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

---
+++ spec/overcommit/hook/pre_commit/pylint_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::Pylint do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+
+  before do
+    subject.stub(:applicable_files).and_return(%w[file1.py file2.py])
+  end
+
+  context 'when pylint exits successfully' do
+    before do
+      result = double('result')
+      result.stub(:success?).and_return(true)
+      subject.stub(:execute).and_return(result)
+    end
+
+    it { should pass }
+  end
+
+  context 'when pylint exits unsucessfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(false)
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'and it reports a warning' do
+      before do
+        result.stub(:stdout).and_return([
+          'file1.py:2:C: Missing function docstring (missing-docstring)'
+        ].join("\n"))
+
+        subject.stub(:modified_lines_in_file).and_return([2, 3])
+      end
+
+      it { should warn }
+    end
+
+    context 'and it reports an error' do
+      before do
+        result.stub(:stdout).and_return([
+          "file1.py:2:E: Instance of 'Foo' has no 'bar' member (no-member)"
+        ].join("\n"))
+
+        subject.stub(:modified_lines_in_file).and_return([1, 2])
+      end
+
+      it { should fail_hook }
+    end
+  end
+end
