852e6895623713cefea44db3026bd65c910b6e0d
Josh Hagins <hagins.josh@gmail.com>
2015-02-27 13:54:56 -0500

    Parse errors and warnings in CssLint output
    Change-Id: Iab4500b6208c69df10e5fe52902ab29940f82b5b
    Reviewed-on: http://gerrit.causes.com/47143
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/overcommit/hook/pre_commit/css_lint.rb
+++ lib/overcommit/hook/pre_commit/css_lint.rb
@@ -1,11 +1,29 @@
 module Overcommit::Hook::PreCommit
   # Runs `csslint` against any modified CSS files.
   class CssLint < Base
+    MESSAGE_REGEX = /
+      ^(?<file>[^:]+):\s
+      (?:line\s(?<line>\d+)[^EW]+)?
+      (?<type>Error|Warning)
+    /x
+
     def run
       result = execute(command + applicable_files)
-      return :pass if result.stdout !~ /Error - (?!Unknown @ rule)/
+      output = result.stdout.chomp
+      return :pass if result.success? && output.empty?
 
-      [:fail, result.stdout]
+      extract_messages(
+        output.split("\n").collect(&method(:add_line_number)),
+        MESSAGE_REGEX,
+        lambda { |type| type.downcase.to_sym }
+      )
+    end
+
+    private
+
+    # Hack to include messages that apply to the entire file
+    def add_line_number(message)
+      message.sub(/(?<!\d,\s)(Error|Warning)/, 'line 0, \1')
     end
   end
 end
