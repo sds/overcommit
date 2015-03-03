4573d151d4704f66b2c8d2599e51559f2ec7f4b8
Josh Hagins <hagins.josh@gmail.com>
2015-03-03 12:39:40 -0500

    Extract errors and warnings from coffeelint output
    Use 'raw' JSON reporter to extract information from coffeelint
    messages. There is no one-line reporter, so this is the best we
    can do. There is a 'csv' reporter, but it's not as safe since
    syntax error messages include newlines and unicode.

--- lib/overcommit/hook/pre_commit/coffee_lint.rb
+++ lib/overcommit/hook/pre_commit/coffee_lint.rb
@@ -1,11 +1,40 @@
+require 'json'
+
 module Overcommit::Hook::PreCommit
   # Runs `coffeelint` against any modified CoffeeScript files.
   class CoffeeLint < Base
     def run
       result = execute(command + applicable_files)
-      return :pass if result.success?
 
-      [:fail, result.stdout]
+      begin
+        parse_json_messages(result.stdout)
+      rescue JSON::ParserError => e
+        [:fail, "Error parsing coffeelint output: #{e.message}"]
+      end
+    end
+
+    private
+
+    def parse_json_messages(output)
+      JSON.parse(output).collect do |file, messages|
+        messages.collect { |msg| extract_message(file, msg) }
+      end.flatten
+    end
+
+    def extract_message(file, message_hash)
+      type = message_hash['level'].include?('w') ? :warning : :error
+      line = message_hash['lineNumber']
+      rule = message_hash['rule']
+      msg = message_hash['message']
+      text =
+        if rule == 'coffeescript_error'
+          # Syntax errors are output in different format.
+          # Splice in the file name and grab the first line.
+          msg.sub('[stdin]', file).split("\n")[0]
+        else
+          "#{file}:#{line}: #{msg} (#{rule})"
+        end
+      Overcommit::Hook::Message.new(type, file, line, text)
     end
   end
 end
