29f319ecf982ef892f47dab0959502b84760e79a
Josh Hagins <hagins.josh@gmail.com>
2015-03-03 15:18:09 -0500

    Extract file and line info from golint messages
    Change-Id: Ic4aedfe0c62197726a32fcca6de8ff2cc66039cd
    Reviewed-on: http://gerrit.causes.com/47521
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/overcommit/hook/pre_commit/go_lint.rb
+++ lib/overcommit/hook/pre_commit/go_lint.rb
@@ -3,10 +3,16 @@ module Overcommit::Hook::PreCommit
   class GoLint < Base
     def run
       result = execute(command + applicable_files)
+      output = result.stdout
       # Unfortunately the exit code is always 0
-      return :pass if result.stdout.empty?
+      return :pass if output.empty?
 
-      [:fail, result.stdout]
+      # example message:
+      #   path/to/file.go:1:1: Error message
+      extract_messages(
+        output.split("\n"),
+        /^(?<file>[^:]+):(?<line>\d+)/
+      )
     end
   end
 end
