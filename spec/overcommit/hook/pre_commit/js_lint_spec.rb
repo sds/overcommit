5373a43f69953c568adf9d0febd798cb718a9881
Josh Hagins <hagins.josh@gmail.com>
2015-03-08 19:31:58 -0400

    Add pre-commit hook for JSLint
    Change-Id: Ifb3686d5913af5ced4258fcca3b717c3d03041fd
    Reviewed-on: http://gerrit.causes.com/47522
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

---
+++ spec/overcommit/hook/pre_commit/js_lint_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::JsLint do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+
+  before do
+    subject.stub(:applicable_files).and_return(%w[file1.js file2.js])
+  end
+
+  context 'when jslint exits successfully' do
+    before do
+      result = double('result')
+      result.stub(:success?).and_return(true)
+      subject.stub(:execute).and_return(result)
+    end
+
+    it { should pass }
+  end
+
+  context 'when jslint exits unsucessfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(false)
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'and it reports an error' do
+      before do
+        result.stub(:stdout).and_return([
+          "file1.js:1:3: Expected ']' at column 9, not column 3."
+        ].join("\n"))
+      end
+
+      it { should fail_hook }
+    end
+  end
+end
