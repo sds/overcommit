fa88e74b07ef3ffc09b429506bc5256a68915bba
Joe Lencioni <joe.lencioni@brigade.com>
2015-03-13 09:04:52 -0400

    Fix comment in CapitalizedSubject hook
    This was leftover from the hook I copied when creating it.
    
    Change-Id: I3be089503b7eaf90f256d2ce03a10bf8fec7b1f4
    Reviewed-on: http://gerrit.causes.com/47740
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Joe Lencioni <joe.lencioni@brigade.com>

--- lib/overcommit/hook/commit_msg/capitalized_subject.rb
+++ lib/overcommit/hook/commit_msg/capitalized_subject.rb
@@ -1,5 +1,5 @@
 module Overcommit::Hook::CommitMsg
-  # Ensures commit message subject lines are followed by a blank line.
+  # Ensures commit message subject lines start with a capital letter.
   class CapitalizedSubject < Base
     def run
       first_letter = commit_message_lines[0].to_s.match(/^[[:punct:]]*(.)/)[1]
