d88c6c7f0cd14817a91c8b0957f528611679b788
Josh Hagins <hagins.josh@gmail.com>
2015-03-18 01:41:58 -0400

    Add pre-commit hook to check for case conflicts
    Files with the same name but different capitalization can cause
    trouble on case-insensitive filesystems.
    
    Change-Id: Ic860f23f28a8c1bc723aac010fab401ad0738125
    Reviewed-on: http://gerrit.causes.com/48150
    Tested-by: jenkins <jenkins@brigade.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

---
+++ lib/overcommit/hook/pre_commit/case_conflicts.rb
@@ -0,0 +1,20 @@
+module Overcommit::Hook::PreCommit
+  # Checks for files that would conflict in case-insensitive filesystems
+  # Adapted from https://github.com/pre-commit/pre-commit-hooks
+  class CaseConflicts < Base
+    def run
+      paths = Set.new(applicable_files.map { |file| File.dirname(file) + File::SEPARATOR })
+      repo_files = Set.new(Overcommit::GitRepo.list_files(paths.to_a) + applicable_files)
+      conflict_hash = repo_files.classify(&:downcase).
+        select { |_, files| files.size > 1 }
+      conflict_files = applicable_files.
+        select { |file| conflict_hash.include?(file.downcase) }
+
+      conflict_files.map do |file|
+        conflicts = conflict_hash[file.downcase].map { |f| File.basename(f) }
+        msg = "Conflict detected for case-insensitive file systems: #{conflicts.join(', ')}"
+        Overcommit::Hook::Message.new(:error, file, nil, msg)
+      end
+    end
+  end
+end
---
+++ spec/overcommit/hook/pre_commit/case_conflicts_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::CaseConflicts do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+
+  before do
+    Overcommit::GitRepo.stub(:list_files).and_return(%w[foo])
+  end
+
+  context 'when a new file conflicts with an existing file' do
+    before do
+      subject.stub(:applicable_files).and_return(%w[Foo])
+    end
+
+    it { should fail_hook }
+  end
+
+  context 'when a new file conflicts with another new file' do
+    before do
+      subject.stub(:applicable_files).and_return(%w[bar Bar])
+    end
+
+    it { should fail_hook }
+  end
+
+  context 'when there are no conflicts' do
+    before do
+      subject.stub(:applicable_files).and_return(%w[bar baz])
+    end
+
+    it { should pass }
+  end
+end
