ecb862d7fb6c5fe8af7d84b8ab0769d413423165
Josh Hagins <hagins.josh@gmail.com>
2015-04-02 04:26:22 -0400

    Add pre_push base hook

---
+++ lib/overcommit/hook/pre_push/base.rb
@@ -0,0 +1,10 @@
+require 'forwardable'
+
+module Overcommit::Hook::PrePush
+  # Functionality common to all pre-push hooks.
+  class Base < Overcommit::Hook::Base
+    extend Forwardable
+
+    def_delegators :@context, :remote_name, :remote_url, :pushed_commits
+  end
+end
