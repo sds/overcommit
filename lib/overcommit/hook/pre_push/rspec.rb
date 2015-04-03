74122b0e725b50580dbd0461bd0b7ae6d8c9153c
Josh Hagins <hagins.josh@gmail.com>
2015-04-03 17:22:29 -0400

    Add Rspec pre-push hook

---
+++ lib/overcommit/hook/pre_push/rspec.rb
@@ -0,0 +1,12 @@
+module Overcommit::Hook::PrePush
+  # Runs `rspec` test suite before push
+  class Rspec < Base
+    def run
+      result = execute(command)
+      return :pass if result.success?
+
+      output = result.stdout + result.stderr
+      [:fail, output]
+    end
+  end
+end
