a98b46cc58e0273495166c8f1c74c33e424d920d
Josh Hagins <hagins.josh@gmail.com>
2015-04-06 22:02:41 -0400

    Cache rebased_branch and rebased_commits

--- lib/overcommit/hook_context/pre_rebase.rb
+++ lib/overcommit/hook_context/pre_rebase.rb
@@ -7,9 +7,11 @@ module Overcommit::HookContext
       @args[0]
     end
 
-    # Returns the name of the branch being rebased.
+    # Returns the name of the branch being rebased. Empty if rebasing a
+    # detached HEAD.
     def rebased_branch
-      @args[1] || `git symbolic-ref --short --quiet HEAD`.chomp
+      @rebased_branch ||=
+        @args[1] || `git symbolic-ref --short --quiet HEAD`.chomp
     end
 
     # Returns whether we are rebasing a detached HEAD rather than a branch
@@ -25,8 +27,9 @@ module Overcommit::HookContext
     # Returns the SHA1-sums of the series of commits to be rebased
     # in reverse topological order.
     def rebased_commits
-      `git rev-list --topo-order --reverse #{upstream_branch}..#{rebased_branch}`.
-        split("\n")
+      @rebased_commits ||=
+        `git rev-list --topo-order --reverse #{upstream_branch}..#{rebased_branch}`.
+          split("\n")
     end
   end
 end
