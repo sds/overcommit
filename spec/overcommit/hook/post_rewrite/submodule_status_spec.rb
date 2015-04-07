0ee81c807241c098c69e8274d89277ebf535fa2e
Josh Hagins <hagins.josh@gmail.com>
2015-04-07 02:44:47 -0400

    Update specs with changes to SubmoduleStatus hooks

--- spec/overcommit/hook/post_rewrite/submodule_status_spec.rb
+++ spec/overcommit/hook/post_rewrite/submodule_status_spec.rb
@@ -5,33 +5,49 @@ describe Overcommit::Hook::PostRewrite::SubmoduleStatus do
   let(:context) { double('context') }
   subject { described_class.new(config, context) }
 
-  let(:result) { double('result') }
+  let(:submodule_status) { double('submodule_status') }
 
   before do
-    result.stub(:stdout).and_return("#{prefix}#{random_hash} sub (heads/master)")
-    subject.stub(:execute).and_return(result)
+    submodule_status.stub(:path).and_return('sub')
+    subject.stub(:submodule_statuses).and_return([submodule_status])
   end
 
   context 'when submodule is up to date' do
-    let(:prefix) { '' }
+    before do
+      submodule_status.stub(uninitialized?: false,
+                            outdated?: false,
+                            merge_conflict?: false)
+    end
 
     it { should pass }
   end
 
   context 'when submodule is uninitialized' do
-    let(:prefix) { '-' }
+    before do
+      submodule_status.stub(uninitialized?: true,
+                            outdated?: false,
+                            merge_conflict?: false)
+    end
 
     it { should warn(/uninitialized/) }
   end
 
   context 'when submodule is outdated' do
-    let(:prefix) { '+' }
+    before do
+      submodule_status.stub(uninitialized?: false,
+                            outdated?: true,
+                            merge_conflict?: false)
+    end
 
     it { should warn(/out of date/) }
   end
 
   context 'when submodule has merge conflicts' do
-    let(:prefix) { 'U' }
+    before do
+      submodule_status.stub(uninitialized?: false,
+                            outdated?: false,
+                            merge_conflict?: true)
+    end
 
     it { should warn(/merge conflicts/) }
   end
