40384fce79bb7aa832e893ef5cd3ada45d0ca6dc
Shane da Silva <shane.dasilva@brigade.com>
2015-04-10 05:11:23 +0000

    Add GIT_VERSION magic constant
    Add a constant which makes it convenient to execute code based on the
    version of git we're running. Returning the `Overcommit::Utils::Version`
    class, doing a comparison is now as simple as writing:
    
        if GIT_VERSION < '1.8.5'
          ...
        end

---
+++ lib/overcommit/git_version.rb
@@ -0,0 +1,15 @@
+# Returns the version of the available git binary.
+#
+# This is intended to be used to conveniently execute code based on a specific
+# git version. Simply compare to a version string:
+#
+# @example
+#   if GIT_VERSION <= '1.8.5'
+#     ...
+#   end
+module Overcommit
+  GIT_VERSION = begin
+    version = `git --version`.chomp[/[\d\.]+/, 0]
+    Overcommit::Utils::Version.new(version)
+  end
+end
