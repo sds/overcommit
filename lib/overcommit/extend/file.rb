990aa5ba61857ce0e61b4ff7894f1fe04bf856a3
Josh Hagins <hagins.josh@gmail.com>
2015-04-15 23:23:14 -0400

    Monkeypatch File class for Windows mklink support
    `File.symlink` is not implemented for Windows in the core library.
    Here we override `File.symlink` and `File.symlink?` to use the
    Windows `mklink` command as an alternative.
    
    We also override `File.readlink` to add Windows compatibility.

---
+++ lib/overcommit/extend/file.rb
@@ -0,0 +1,51 @@
+require 'overcommit/os'
+require 'overcommit/subprocess'
+
+# Adapted from http://stackoverflow.com/a/22716582
+class << File
+  alias_method :old_symlink, :symlink
+  alias_method :old_symlink?, :symlink?
+  alias_method :old_readlink, :readlink
+
+  def symlink(old_name, new_name)
+    return old_symlink(old_name, new_name) unless Overcommit::OS.windows?
+
+    result = win32_mklink_cmd(old_name, new_name)
+    result.status
+  end
+
+  def symlink?(file_name)
+    return old_symlink?(file_name) unless Overcommit::OS.windows?
+
+    result = win32_dir_cmd(file_name)
+    !(result.stdout =~ /<SYMLINK>/).nil?
+  end
+
+  def readlink(link_name)
+    return old_readlink(link_name) unless Overcommit::OS.windows?
+
+    result = win32_dir_cmd(link_name)
+
+    unless result.stdout =~ /<SYMLINK>/
+      raise ArgumentError, "#{link_name} is not a symlink"
+    end
+
+    # Extract symlink target from output, which looks like:
+    #   11/13/2012 12:53 AM <SYMLINK> mysymlink [C:\Windows\Temp\somefile.txt]
+    result.stdout[/\[.+\]/][1..-2]
+  end
+
+  private
+
+  def win32_dir_cmd(file_name)
+    Overcommit::Subprocess.spawn(
+      %W[cmd.exe /c dir #{file_name}]
+    )
+  end
+
+  def win32_mklink_cmd(old_name, new_name)
+    Overcommit::Subprocess.spawn(
+      %W[cmd.exe /c mklink #{new_name} #{old_name}]
+    )
+  end
+end
