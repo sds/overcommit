a075fe551bfd100e6f58f9175a01ed98858f4937
Josh Hagins <hagins.josh@gmail.com>
2015-04-16 18:21:17 -0400

    Use Subprocess.spawn instead of Open3.popen3

--- lib/overcommit/extend/file.rb
+++ lib/overcommit/extend/file.rb
@@ -1,4 +1,5 @@
-require 'open3'
+require 'overcommit/os'
+require 'overcommit/subprocess'
 
 # Adapted from http://stackoverflow.com/a/22716582
 class << File
@@ -6,22 +7,18 @@ class << File
   alias_method :old_symlink?, :symlink?
 
   def symlink(old_name, new_name)
-    # if on windows, call mklink, else self.symlink
     if Overcommit::OS.windows?
-      _stdin, _stdout, _stderr, wait_thr =
-        Open3.popen3('cmd.exe', "/c mklink #{new_name} #{old_name}")
-      wait_thr.value.exitstatus
+      result = Overcommit::Subprocess.spawn('cmd.exe', "/c mklink #{new_name} #{old_name}")
+      result.status
     else
       old_symlink(old_name, new_name)
     end
   end
 
   def symlink?(file_name)
-    # if on windows, call mklink, else self.symlink
     if Overcommit::OS.windows?
-      _stdin, _stdout, _stderr, wait_thr =
-        Open3.popen3("cmd.exe /c dir #{file_name} | find \"SYMLINK\"")
-      wait_thr.value.exitstatus
+      result = Overcommit::Subprocess.spawn('cmd.exe', "/c dir #{file_name} | find \"SYMLINK\"")
+      result.success?
     else
       old_symlink?(file_name)
     end
