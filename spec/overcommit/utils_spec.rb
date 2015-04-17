b311c4d7e2c7a6bba080adb58b43de964a7a470f
Josh Hagins <hagins.josh@gmail.com>
2015-04-16 21:32:35 -0400

    Fix for cross-platform echo compatibiliity

--- spec/overcommit/utils_spec.rb
+++ spec/overcommit/utils_spec.rb
@@ -207,7 +207,7 @@ describe Overcommit::Utils do
   end
 
   describe '.execute_in_background' do
-    let(:arguments) { %w[(echo Hello World) > some-file] }
+    let(:arguments) { Overcommit::OS.windows? ? %w[echo null>some-file] : %w[touch some-file] }
     subject { described_class.execute_in_background(arguments) }
 
     around do |example|
@@ -218,7 +218,7 @@ describe Overcommit::Utils do
 
     it 'executes the command' do
       wait_until { subject.exited? } # Make sure process terminated before checking
-      File.read('some-file').should == "Hello World\n"
+      File.exist?('some-file').should == true
     end
   end
 
