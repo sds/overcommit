e4eb7189511226fc83247ba65efce77eb8d166a7
Josh Hagins <hagins.josh@gmail.com>
2015-04-18 17:39:22 -0400

    Fix unescaped newline in single-quoted string

--- lib/overcommit/hook/pre_commit/author_name.rb
+++ lib/overcommit/hook/pre_commit/author_name.rb
@@ -7,8 +7,8 @@ module Overcommit::Hook::PreCommit
 
       unless name.split(' ').count >= 2
         return :fail,
-               "Author must have at least first and last name, but was: #{name}." \
-               '\nSet your name with `git config --global user.name "Your Name"`'
+               "Author must have at least first and last name, but was: #{name}.\n" \
+               'Set your name with `git config --global user.name "Your Name"`'
       end
 
       :pass
