ed378484c2388b49b54dbdec1266c523ef6127e9
Josh Hagins <hagins.josh@gmail.com>
2015-04-18 17:00:02 -0400

    Use early returns in File monkeypatch methods

--- lib/overcommit/extend/file.rb
+++ lib/overcommit/extend/file.rb
@@ -8,37 +8,31 @@ class << File
   alias_method :old_readlink, :readlink
 
   def symlink(old_name, new_name)
-    if Overcommit::OS.windows?
-      result = win32_mklink_cmd(old_name, new_name)
-      result.status
-    else
-      old_symlink(old_name, new_name)
-    end
+    return old_symlink(old_name, new_name) unless Overcommit::OS.windows?
+
+    result = win32_mklink_cmd(old_name, new_name)
+    result.status
   end
 
   def symlink?(file_name)
-    if Overcommit::OS.windows?
-      result = win32_dir_cmd(file_name)
-      !(result.stdout =~ /<SYMLINK>/).nil?
-    else
-      old_symlink?(file_name)
-    end
+    return old_symlink?(file_name) unless Overcommit::OS.windows?
+
+    result = win32_dir_cmd(file_name)
+    !(result.stdout =~ /<SYMLINK>/).nil?
   end
 
   def readlink(link_name)
-    if Overcommit::OS.windows?
-      result = win32_dir_cmd(link_name)
+    return old_readlink(link_name) unless Overcommit::OS.windows?
 
-      unless result.stdout =~ /<SYMLINK>/
-        raise ArgumentError, "#{link_name} is not a symlink"
-      end
+    result = win32_dir_cmd(link_name)
 
-      # Extract symlink target from output, which looks like:
-      #   11/13/2012 12:53 AM <SYMLINK> mysymlink [C:\Windows\Temp\somefile.txt]
-      result.stdout[/\[.+\]/][1..-2]
-    else
-      old_readlink(link_name)
+    unless result.stdout =~ /<SYMLINK>/
+      raise ArgumentError, "#{link_name} is not a symlink"
     end
+
+    # Extract symlink target from output, which looks like:
+    #   11/13/2012 12:53 AM <SYMLINK> mysymlink [C:\Windows\Temp\somefile.txt]
+    result.stdout[/\[.+\]/][1..-2]
   end
 
   private
