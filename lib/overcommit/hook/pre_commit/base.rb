2570054e699f90078e18ca75245d6e6e5dfd5e79
Josh Hagins <hagins.josh@gmail.com>
2015-04-24 12:34:39 -0400

    Allow message regex to omit 'line' group

--- lib/overcommit/hook/pre_commit/base.rb
+++ lib/overcommit/hook/pre_commit/base.rb
@@ -32,7 +32,7 @@ module Overcommit::Hook::PreCommit
         end
 
         file = extract_file(match, message)
-        line = extract_line(match, message) unless match[:line].nil?
+        line = extract_line(match, message) if match.names.include?('line') && match[:line]
         type = extract_type(match, message, type_categorizer)
 
         Overcommit::Hook::Message.new(type, file, line, message)
