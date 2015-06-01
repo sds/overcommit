d898ff5d485869f30f8f5acc2de74ef0a39664c3
Josh Hagins <hagins.josh@gmail.com>
2015-06-01 16:00:43 -0400

    Check against existing files only if not initial commit
    Otherwise you get 'fatal: Not a valid object name HEAD' since
    `git ls-tree` requires an existing rev.

--- lib/overcommit/hook/pre_commit/case_conflicts.rb
+++ lib/overcommit/hook/pre_commit/case_conflicts.rb
@@ -3,8 +3,13 @@ module Overcommit::Hook::PreCommit
   # Adapted from https://github.com/pre-commit/pre-commit-hooks
   class CaseConflicts < Base
     def run
-      paths = Set.new(applicable_files.map { |file| File.dirname(file) + File::SEPARATOR })
-      repo_files = Set.new(Overcommit::GitRepo.list_files(paths.to_a) + applicable_files)
+      repo_files = Set.new(applicable_files)
+
+      unless Overcommit::GitRepo.initial_commit?
+        paths = repo_files.map { |file| File.dirname(file) + File::SEPARATOR }
+        repo_files += Overcommit::GitRepo.list_files(paths)
+      end
+
       conflict_hash = repo_files.classify(&:downcase).
         select { |_, files| files.size > 1 }
       conflict_files = applicable_files.
