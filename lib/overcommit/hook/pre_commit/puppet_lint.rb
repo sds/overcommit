bba3822093fa4df21f7a27a3f82cb9e3deb40cbe
Josh Hagins <hagins.josh@gmail.com>
2015-07-16 00:07:02 -0400

    Add pre-commit hook for puppet-lint
    Add spec for PuppetLint pre-commit hook

---
+++ lib/overcommit/hook/pre_commit/puppet_lint.rb
@@ -0,0 +1,24 @@
+module Overcommit::Hook::PreCommit
+  # Runs 'puppet-lint' against any modified Puppet files.
+  #
+  # @see http://puppet-lint.com/
+  class PuppetLint < Base
+    MESSAGE_REGEX = /(?<file>.+):(?<line>\d+):\d+:(?<type>\w+)/
+
+    MESSAGE_TYPE_CATEGORIZER = lambda do |type|
+      type == 'ERROR' ? :error : :warning
+    end
+
+    def run
+      result = execute(command + applicable_files)
+      output = result.stdout.chomp.gsub(/^"|"$/, '')
+      return :pass if result.success? && output.empty?
+
+      extract_messages(
+        output.split("\n"),
+        MESSAGE_REGEX,
+        MESSAGE_TYPE_CATEGORIZER
+      )
+    end
+  end
+end
