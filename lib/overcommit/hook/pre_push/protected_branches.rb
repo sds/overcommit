87f38fce3c4cb8ea7fdcf19b311376633c917871
Josh Hagins <hagins.josh@gmail.com>
2015-07-30 16:34:31 -0400

    ProtectedBranches: log pushed_refs in debug mode

--- lib/overcommit/hook/pre_push/protected_branches.rb
+++ lib/overcommit/hook/pre_push/protected_branches.rb
@@ -2,6 +2,7 @@ module Overcommit::Hook::PrePush
   # Prevents destructive updates to specified branches.
   class ProtectedBranches < Base
     def run
+      log_pushed_refs
       return :pass unless illegal_pushes.any?
 
       messages = illegal_pushes.map do |pushed_ref|
@@ -12,6 +13,15 @@ module Overcommit::Hook::PrePush
     end
 
     private
+
+    def log_pushed_refs
+      log_msg = "\nPushed refs:\n\t#{pushed_refs_str}"
+      Overcommit::Utils.log.debug(log_msg)
+    end
+
+    def pushed_refs_str
+      pushed_refs.map(&:to_s).join("\t\n")
+    end
 
     def branches
       @branches ||= config['branches']
