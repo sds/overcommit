ed26f3c42d844ff689a8beaa6bc78675506f567f
Dan Rabinowitz <danrabinowitz@Dan-Rabinowitzs-LS-MBP.local>
2015-11-30 08:41:30 -0700

    Add minitest pre-push hook

---
+++ spec/overcommit/hook/pre_push/minitest_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PrePush::Minitest do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+
+  context 'when minitest exits successfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(true)
+      subject.stub(:execute).and_return(result)
+    end
+
+    it { should pass }
+  end
+
+  context 'when minitest exits unsuccessfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(false)
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'with a runtime error' do
+      before do
+        result.stub(stdout: '', stderr: <<-EOS)
+          1) Error:
+          FooTest#test_: foo should bar. :
+          RuntimeError:
+          test/model/foo_test.rb:1:in `block (2 levels) in <class:FooTest>'
+        EOS
+      end
+
+      it { should fail_hook }
+    end
+  end
+end
