f4ea8bcd206d2370ba3790f225867498b8aa8806
Josh Hagins <hagins.josh@gmail.com>
2016-01-17 19:25:45 -0500

    Add mdl pre-commit hook
    Analyze Markdown files with [mdl/markdownlint][mdl]
    
    [mdl]: https://github.com/mivok/markdownlint

---
+++ lib/overcommit/hook/pre_commit/mdl.rb
@@ -0,0 +1,23 @@
+module Overcommit::Hook::PreCommit
+  # Runs `mdl` against any modified Markdown files
+  #
+  # @see https://github.com/mivok/markdownlint
+  class Mdl < Base
+    MESSAGE_REGEX = /^(?<file>(?:\w:)?[^:]+):(?<line>\d+)/
+
+    def run
+      result = execute(command, args: applicable_files)
+      output = result.stdout.chomp
+
+      return :pass if result.success?
+      return [:fail, result.stderr] unless result.stderr.empty?
+
+      # example message:
+      #   path/to/file.md:1: MD001 Error message
+      extract_messages(
+        output.split("\n"),
+        MESSAGE_REGEX
+      )
+    end
+  end
+end
