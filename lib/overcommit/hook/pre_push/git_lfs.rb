f8bddd73c15e6a9476b0803c65ba8a7e5dd9fb3d
Alex Agranov <alex@morphogenic.net>
2016-04-19 20:05:56 -0400

    adding initial version of a GitLfs PrePush hook

---
+++ lib/overcommit/hook/pre_push/git_lfs.rb
@@ -0,0 +1,43 @@
+module Overcommit::Hook::PrePush
+  # Replaces the pre-push hook installed by `git lfs install` that is then moved aside by
+  # `overcommit --install`
+  #
+  # Enable this hook in .overcommit.yml like so:
+  #
+  #  PrePush:
+  #    GitLfs:
+  #      enabled: true
+  #      command: ['git', 'lfs', 'pre-push']
+  #
+  # @see https://github.com/github/git-lfs
+  class GitLfs < Base
+    # Overcommit expects you to override this method which will be called
+    # everytime your pre-commit hook is run.
+    def run
+      # Create two arrays to hold our error and warning messages.
+      error_lines = []
+      warning_lines = []
+
+      # Check if git-lfs executable exists
+      path_to_git_lfs = `command -v git-lfs`.strip
+      error_lines << "This repository is configured for Git LFS but 'git-lfs' was not found "\
+                     "on your path. If you no longer wish to use Git LFS, disable GitLfs in "\
+                     ".overcommit.yml." unless File.exist?(path_to_git_lfs)
+
+      result = execute(command)
+      error_lines << "'#{command}' returned non-zero exit status: #{result.status}" unless
+        result.success?
+
+      # Overcommit expects 1 of the 3 as return values, `:fail`, `:warn` or `:pass`.
+      # If the hook returns `:fail`, the commit will be aborted with our message
+      # containing the errors.
+      return :fail, error_lines.join("\n") if error_lines.any?
+
+      :pass
+    end
+
+    def command
+      super + ["#{remote_name}", "#{remote_url}"]
+    end
+  end
+end
---
+++ spec/overcommit/hook/pre_push/git_lfs_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PrePush::GitLfs do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context', all_files: ['test/test_foo.rb'], remote_name: 'origin', remote_url: 'https://github.io/brigade/fake_repo') }
+  subject { described_class.new(config, context) }
+
+  context 'when git_lfs exits successfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(true)
+      subject.stub(:execute).and_return(result)
+    end
+
+    it { should pass }
+  end
+
+  context 'when git_lfs exits unsuccessfully' do
+    let(:result) { double('result', status: -1) }
+
+    before do
+      result.stub(:success?).and_return(false)
+      subject.stub(:execute).and_return(result)
+    end
+
+    it { should fail_hook }
+  end
+end
