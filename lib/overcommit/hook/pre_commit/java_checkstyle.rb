b27c337ba08d3151e20665534638fe7c32cb2190
Gautam Jain <gautamjain@gmail.com>
2016-06-29 15:57:54 -0700

    Update JavaCheckstyle to categorize messages depending on type
    Checkstyle generates messages with `ERROR`, `WARN`, or `INFO` tags depending on their severity. Prior to this, all messages (irrespective of their tag) were treated as an error. With this change, messages with `WARN` and `INFO` tags will be handled as Overcommit warnings.

--- lib/overcommit/hook/pre_commit/java_checkstyle.rb
+++ lib/overcommit/hook/pre_commit/java_checkstyle.rb
@@ -3,18 +3,25 @@ module Overcommit::Hook::PreCommit
   #
   # @see http://checkstyle.sourceforge.net/
   class JavaCheckstyle < Base
-    MESSAGE_REGEX = /^(\[[^\]]+\]\s+)?(?<file>(?:\w:)?[^:]+):(?<line>\d+)/
+    MESSAGE_REGEX = /^(?<type>\[[^\]]+\]\s+)?(?<file>(?:\w:)?[^:]+):(?<line>\d+)/
+
+    MESSAGE_TYPE_CATEGORIZER = lambda do |type|
+      # Type may be nil if checkstyle doesn't output a 'tag'
+      type = type || ""
+
+      type.include?('WARN') || type.include?('INFO') ? :warning : :error
+    end
 
     def run
       result = execute(command, args: applicable_files)
       output = result.stdout.chomp
-      return :pass if result.success?
 
       # example message:
       #   path/to/file.java:3:5: Error message
       extract_messages(
         output.split("\n").grep(MESSAGE_REGEX),
-        MESSAGE_REGEX
+        MESSAGE_REGEX,
+        MESSAGE_TYPE_CATEGORIZER
       )
     end
   end
