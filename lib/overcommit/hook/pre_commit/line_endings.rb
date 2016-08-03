f43743b99c9ac2377955959c36504eea5d0a8568
Harm-Jan Blok <Harm-Jan.Blok@swisscom.com>
2016-08-03 10:37:00 +0200

    Added pre-commit hook to check for line endings
    Depending on the configured representation, this hook will verify if
    only unix line endings (LF) or only windows line endings (CRLF) are
    used.

---
+++ lib/overcommit/hook/pre_commit/line_endings.rb
@@ -0,0 +1,51 @@
+module Overcommit::Hook::PreCommit
+  # Checks for line endings in files.
+  class LineEndings < Base
+    def run
+      messages = []
+
+      offending_files.map do |file_name|
+        file = File.open(file_name)
+        file.each_line do |line|
+          # remove configured line-ending
+          line.gsub!(/#{config['eol']}/, '')
+
+          # detect any left over line-ending characters
+          next unless line.end_with?("\n", "\r")
+
+          messages << Overcommit::Hook::Message.new(
+            :error,
+            file_name,
+            file.lineno,
+            "#{file_name}:#{file.lineno}:#{line.inspect}"
+          )
+        end
+      end
+
+      messages
+    end
+
+    private
+
+    def offending_files
+      result = execute(%w[git ls-files --eol -z --], args: applicable_files)
+      raise 'Unable to access git tree' unless result.success?
+
+      result.stdout.split("\0").map do |file_info|
+        i, _w, _attr, path = file_info.split
+        next if i == 'l/-text' # ignore binary files
+        next if i == "l/#{eol}"
+        path
+      end.compact
+    end
+
+    def eol
+      @eol ||=  case config['eol']
+                when "\n"
+                  'lf'
+                when "\r\n"
+                  'crlf'
+                end
+    end
+  end
+end
---
+++ spec/overcommit/hook/pre_commit/line_endings_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::LineEndings do
+  let(:config) do
+    Overcommit::ConfigurationLoader.default_configuration.merge(
+      Overcommit::Configuration.new(
+        'PreCommit' => {
+          'LineEndings' => {
+            'eol' => eol
+          }
+        }
+      )
+    )
+  end
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+  let(:eol) { "\n" }
+  let(:staged_file) { 'filename.txt' }
+
+  before do
+    subject.stub(:applicable_files).and_return([staged_file])
+  end
+
+  around do |example|
+    repo do
+      File.open(staged_file, 'w') { |f| f.write(contents) }
+      `git add #{staged_file}`
+      example.run
+    end
+  end
+
+  context 'when enforcing \n' do
+    context 'when file contains \r\n line endings' do
+      let(:contents) { "CR-LF\r\nline\r\nendings\r\n" }
+
+      it { should fail_hook }
+    end
+
+    context 'when file contains \n endings' do
+      let(:contents) { "LF\nline\nendings\n" }
+
+      it { should pass }
+    end
+  end
+
+  context 'when enforcing \r\n' do
+    let(:eol) { "\r\n" }
+
+    context 'when file contains \r\n line endings' do
+      let(:contents) { "CR-LF\r\nline\r\nendings\r\n" }
+
+      it { should pass }
+    end
+
+    context 'when file contains \n line endings' do
+      let(:contents) { "LF\nline\nendings\n" }
+
+      it { should fail_hook }
+    end
+  end
+end
