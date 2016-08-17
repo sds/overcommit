04b62252af06cc45441f3c149f6aafc1ffa1aba4
Harm-Jan Blok <Harm-Jan.Blok@swisscom.com>
2016-08-17 08:43:56 +0200

    Replaced matches with positive matches

--- lib/overcommit/hook/pre_commit/line_endings.rb
+++ lib/overcommit/hook/pre_commit/line_endings.rb
@@ -7,8 +7,8 @@ module Overcommit::Hook::PreCommit
       applicable_files.map do |file_name|
         file = File.open(file_name)
         file.each_line do |line|
-          next if unix? && !line.end_with?("\r\n")
-          next if windows? && line.end_with?("\r\n")
+          next if unix? && line =~ /\A((?!\r).)*\n\z/
+          next if windows? && line =~ /\A.*\r\n\z/
 
           messages << Overcommit::Hook::Message.new(
             :error,
