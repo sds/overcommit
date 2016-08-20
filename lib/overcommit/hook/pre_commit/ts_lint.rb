a0f3645fd12e84f95103f24fc5f9a5ad6d00578e
Jos Huiting <joshuiting4@gmail.com>
2016-08-20 10:09:52 +0200

    #419 - Add TSLint pre-commit hook

---
+++ lib/overcommit/hook/pre_commit/ts_lint.rb
@@ -0,0 +1,18 @@
+module Overcommit::Hook::PreCommit
+  # Runs `tslint` against modified JavaScript files.
+  # @see http://palantir.github.io/tslint/
+  class TsLint < Base
+    def run
+      result = execute(command, args: applicable_files)
+      output = result.stdout.chomp
+      return :pass if result.success? && output.empty?
+
+      # example message:
+      # src/file/anotherfile.ts[298, 1]: exceeds maximum line length of 140
+      extract_messages(
+          output.split("\n"),
+          /^(?<file>.+?(?=\[))[^\d]+(?<line>\d+).*?/
+      )
+    end
+  end
+end
---
+++ spec/overcommit/hook/pre_commit/ts_lint_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::TsLint do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+
+  before do
+    subject.stub(:applicable_files).and_return(%w[anotherfile.ts])
+  end
+
+  context 'when tslint exits successfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(true)
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'with no output' do
+      before do
+        result.stub(:stdout).and_return('')
+      end
+
+      it { should pass }
+    end
+
+  end
+
+  context 'when tslint exits unsucessfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(false)
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'and it reports an error' do
+      before do
+        result.stub(:stdout).and_return([
+          'src/file/anotherfile.ts[298, 1]: exceeds maximum line length of 140'
+        ].join("\n"))
+      end
+
+      it { should fail_hook }
+    end
+  end
+end
