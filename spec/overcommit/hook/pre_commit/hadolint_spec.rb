162a992e5825f41bfd15b9122b85e69090879e30
Igor Rzegocki <igor@rzegocki.pl>
2017-04-20 21:37:33 +0200

    Add pre commit hook for hadolint

---
+++ spec/overcommit/hook/pre_commit/hadolint_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::Hadolint do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  let(:applicable_files) { %w[Dockerfile Dockerfile.web] }
+  subject { described_class.new(config, context) }
+
+  before do
+    subject.stub(:applicable_files).and_return(applicable_files)
+  end
+
+  around do |example|
+    repo do
+      example.run
+    end
+  end
+
+  before do
+    subject.stub(:execute).with(%w[hadolint], args: Array(applicable_files.first)).
+      and_return(result_dockerfile)
+    subject.stub(:execute).with(%w[hadolint], args: Array(applicable_files.last)).
+      and_return(result_dockerfile_web)
+  end
+
+  context 'and has 2 suggestions' do
+    let(:result_dockerfile) do
+      double(
+        success?: false,
+        stdout: <<-EOF
+Dockerfile:5 DL3015 Avoid additional packages by specifying `--no-install-recommends`
+      EOF
+      )
+    end
+    let(:result_dockerfile_web) do
+      double(
+        success?: false,
+        stdout: <<-EOF
+Dockerfile.web:13 DL3020 Use COPY instead of ADD for files and folders
+      EOF
+      )
+    end
+
+    it { should fail_hook }
+  end
+
+  context 'and has single suggestion for double quote' do
+    let(:result_dockerfile) do
+      double(
+        success?: false,
+        stdout: <<-EOF
+Dockerfile:11 SC2086 Double quote to prevent globbing and word splitting.
+      EOF
+      )
+    end
+    let(:result_dockerfile_web) do
+      double(success?: true, stdout: '')
+    end
+
+    it { should fail_hook }
+  end
+
+  context 'and does not have any suggestion' do
+    let(:result_dockerfile) do
+      double(success?: true, stdout: '')
+    end
+    let(:result_dockerfile_web) do
+      double(success?: true, stdout: '')
+    end
+
+    it { should pass }
+  end
+end
