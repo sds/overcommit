737bd628eec70ca48b19c16bb845eb5ba3074090
Thiago Barbato <thiagocbbt@gmail.com>
2017-10-10 19:45:43 -0300

    Correct LicenseFinder repository link
    The 'r' at the end of the repository link was missing.

--- lib/overcommit/hook/pre_commit/license_finder.rb
+++ lib/overcommit/hook/pre_commit/license_finder.rb
@@ -1,6 +1,6 @@
 module Overcommit::Hook::PreCommit
   # Runs LicenseFinder if any of your package manager declaration files have changed
-  # See more about LicenseFinder at https://github.com/pivotal/LicenseFinde
+  # See more about LicenseFinder at https://github.com/pivotal/LicenseFinder
   class LicenseFinder < Base
     def run
       result = execute(command)
