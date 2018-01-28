e29265ee8bdfde4b47a023ece3472802f9666d2d
Taufek Johar <taufek@gmail.com>
2018-01-28 16:39:35 +0800

    Add PhpStan (PHP Static Code Analyser) PreCommit Hook
    Added [PhpStan](https://github.com/phpstan/phpstan) PreCommit Hook.
    
    **Example Files Change**
    
    ```
    // app/foo.php
    <?php
    
    namespace App;
    
    class Foo
    {
        public function __construct($foo, $test)
        {
            $this->foo = $foo;
        }
    }
    
    // app/bar.php
    <?php
    
    namespace App;
    
    class Bar
    {
        public function __construct($foo)
        {
            $this->foo = $foo;
        }
    
        private function testMe()
        {
            return $this->hello;
        }
    }
    ```
    
    **Example Error Messages***
    
    ```
    Running pre-commit hooks
    Analysing PHP codes...............................[PhpStan] FAILED
    Errors on modified lines:
    /app/bar.php:9:Access to an undefined property App\Bar::$foo.
    /app/bar.php:14:Access to an undefined property App\Bar::$hello.
    /app/foo.php:7:Constructor of class App\Foo has an unused parameter $test.
    /app/foo.php:9:Access to an undefined property App\Foo::$foo.
    
    ✗ One or more pre-commit hooks failed
    ```

---
+++ spec/overcommit/hook/pre_commit/php_stan_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::PhpStan do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+
+  before do
+    subject.stub(:applicable_files).and_return(%w[sample.php])
+  end
+
+  context 'when phpstan exits successfully' do
+    before do
+      sample_output = ''
+
+      result = double('result')
+      result.stub(:success?).and_return(true)
+      result.stub(:stdout).and_return(sample_output)
+      result.stub(:status).and_return(0)
+      subject.stub(:execute).and_return(result)
+    end
+
+    it { should pass }
+  end
+
+  context 'when phpstan exits unsuccessfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(false)
+      result.stub(:status).and_return(2)
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'and it reports a warning' do
+      before do
+        sample_output = [
+          '/sample1.php:14:Call to an undefined static method Sample1::where()',
+          '/sample2.php:17:Anonymous function has an unused use $myVariable.'
+        ].join("\n")
+        result.stub(:stdout).and_return(sample_output)
+      end
+
+      it { should fail_hook }
+    end
+  end
+end
