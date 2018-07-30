22229c95854cdd02d19e6e24e20d7df9ac23fea3
Ruffeng <ruben.suet@gmail.com>
2018-07-31 00:36:50 +0200

    Add Stylelint pre-commit hook (#590)
    * Stylelint linter added in pre-hook
    
    * CODE REVIEW

---
+++ lib/overcommit/hook/pre_commit/stylelint.rb
@@ -0,0 +1,21 @@
+module Overcommit::Hook::PreCommit
+  # Runs `stylelint` against any modified CSS file.
+  #
+  # @see https://github.com/stylelint/stylelint
+  class Stylelint < Base
+    # example of output:
+    # index.css: line 4, col 4, error - Expected indentation of 2 spaces (indentation)
+
+    MESSAGE_REGEX = /^(?<file>.+):\D*(?<line>\d).*$/
+
+    def run
+      result = execute(command, args: applicable_files)
+      output = result.stdout.chomp
+      return :pass if result.success? && output.empty?
+      extract_messages(
+        output.split("\n"),
+        MESSAGE_REGEX
+      )
+    end
+  end
+end
---
+++ spec/overcommit/hook/pre_commit/stylelint_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::Stylelint do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+
+  before do
+    subject.stub(:applicable_files).and_return(%w[file1.scss file2.scss])
+  end
+
+  context 'when stylelint exits successfully' do
+    before do
+      result = double('result')
+      result.stub(:success?).and_return(true)
+      result.stub(:stdout).and_return('')
+      subject.stub(:execute).and_return(result)
+    end
+
+    it { should pass }
+  end
+
+  context 'when stylelint exits unsucessfully' do
+    let(:result) { double('result') }
+
+    before do
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'and it reports an error' do
+      before do
+        result.stub(:success?).and_return(false)
+        result.stub(:stdout).and_return([
+          'index.css: line 4, col 4, error - Expected indentation of 2 spaces (indentation)',
+          'form.css: line 10, col 6, error - Expected indentation of 4 spaces (indentation)',
+        ].join("\n"))
+      end
+
+      it { should fail_hook }
+    end
+  end
+end
