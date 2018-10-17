6bf7c91886b215045408b25409afe1139885d947
David <davidgengenbach@users.noreply.github.com>
2018-10-17 04:59:56 +0200

    Add support for new TsLint output format (#601)

--- lib/overcommit/hook/pre_commit/ts_lint.rb
+++ lib/overcommit/hook/pre_commit/ts_lint.rb
@@ -2,16 +2,24 @@ module Overcommit::Hook::PreCommit
   # Runs `tslint` against modified TypeScript files.
   # @see http://palantir.github.io/tslint/
   class TsLint < Base
+    # example message:
+    # "src/file/anotherfile.ts[298, 1]: exceeds maximum line length of 140"
+    # or
+    # "ERROR: src/AccountController.ts[4, 28]: expected call-signature to have a typedef"
+    MESSAGE_REGEX = /^(?<type>.+: )?(?<file>.+?(?=\[))[^\d]+(?<line>\d+).*?/
+
     def run
       result = execute(command, args: applicable_files)
       output = result.stdout.chomp
       return :pass if result.success? && output.empty?
 
-      # example message:
-      # src/file/anotherfile.ts[298, 1]: exceeds maximum line length of 140
+      output_lines = output.split("\n").map(&:strip).reject(&:empty?)
+      type_categorizer = ->(type) { type.nil? || type.include?('ERROR') ? :error : :warning }
+
       extract_messages(
-          output.split("\n"),
-          /^(?<file>.+?(?=\[))[^\d]+(?<line>\d+).*?/
+          output_lines,
+          MESSAGE_REGEX,
+          type_categorizer
       )
     end
   end
