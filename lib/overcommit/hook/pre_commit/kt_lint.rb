1dc4ee521065cd37feab012e94cbd1121e3193c3
Ensar Sarajčić <es.ensar@gmail.com>
2018-10-27 00:40:42 +0200

    KtLint: Pre-commit hook
    * Add KtLint pre-commit hook
    * Update README

---
+++ lib/overcommit/hook/pre_commit/kt_lint.rb
@@ -0,0 +1,17 @@
+module Overcommit::Hook::PreCommit
+  # Runs `ktlint` against modified Kotlin files.
+  # @see https://github.com/shyiko/ktlint
+  class KtLint < Base
+    MESSAGE_REGEX = /((?<file>[^:]+):(?<line>\d+):(\d+):(?<message>.+))/
+
+    def run
+      result = execute(command, args: applicable_files)
+      return :pass if result.success?
+
+      extract_messages(
+        result.stdout.split("\n").grep(MESSAGE_REGEX),
+        MESSAGE_REGEX
+      )
+    end
+  end
+end
---
+++ spec/overcommit/hook/pre_commit/kt_lint_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Overcommit::Hook::PreCommit::KtLint do
+  let(:config)  { Overcommit::ConfigurationLoader.default_configuration }
+  let(:context) { double('context') }
+  subject { described_class.new(config, context) }
+
+  before do
+    subject.stub(:applicable_files).and_return(%w[file1.kt file2.kt])
+  end
+
+  context 'when KtLint exits successfully' do
+    before do
+      result = double('result')
+      result.stub(:success?).and_return(true)
+      subject.stub(:execute).and_return(result)
+    end
+
+    it { should pass }
+  end
+
+  context 'when KtLint exits unsucessfully' do
+    let(:result) { double('result') }
+
+    before do
+      result.stub(:success?).and_return(false)
+      subject.stub(:execute).and_return(result)
+    end
+
+    context 'and it reports an error' do
+      before do
+        result.stub(:stdout).and_return([
+          'file1.kt:12:10: error message'
+        ].join("\n"))
+      end
+
+      it { should fail_hook }
+    end
+  end
+end
