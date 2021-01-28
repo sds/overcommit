3e295a995efb785bcb74e48f91a28901cba487ea
Ensar Sarajčić <es.ensar@gmail.com>
2021-01-28 18:19:10 +0100

    Add dartanalyzer pre-commit hook

---
+++ lib/overcommit/hook/pre_commit/dart_analyzer.rb
@@ -0,0 +1,17 @@
+module Overcommit::Hook::PreCommit
+  # Runs `dartanalyzer` against modified Dart files.
+  # @see https://dart.dev/tools/dartanalyzer
+  class DartAnalyzer < Base
+    MESSAGE_REGEX = /.*•\ (?<message>[^•]+)•\ (?<file>[^:]+):(?<line>\d+):(\d+)\.*/
+
+    def run
+      result = execute(command, args: applicable_files)
+      return :pass if result.success?
+
+      extract_messages(
+        result.stdout.split("\n").grep(MESSAGE_REGEX),
+        MESSAGE_REGEX
+      )
+    end
+  end
+end
